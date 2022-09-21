54729088f11c0f15166ec526684a21e070e9fa3d
cks3066 <cks3066@naver.com>
2022-09-21 14:09:44 +0900

    feat: 유틸 함수 코드 추가

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,146 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+class DomUtils {
+  #element:
+    | (Element & {
+        innerHTML: () => string;
+        show: () => void;
+        hidden: () => void;
+        addEvent: () => void;
+      })
+    | null = null;
+
+  constructor(selector: string) {
+    this.#element = document.querySelector(selector);
+  }
+
+  getElement() {
+    return this.#element;
+  }
+
+  innerHTML() {
+    if (!this.#element) {
+      throw new Error('innerHTML 에러: element가 null 입니다.');
+    }
+    return this.#element.innerHTML;
+  }
+
+  show() {}
+
+  hidden() {}
+
+  addEvent(
+    event: string,
+    listener: (this: Element, ev: Event) => any,
+    options?: boolean | AddEventListenerOptions | undefined
+  ) {
+    if (!this.#element) {
+      throw new Error('addEvent 에러: element가 null 입니다.');
+    }
+    this.#element.addEventListener(event, listener, options);
+  }
 }
 
+function _(selector: string) {
+  const domUtils = new DomUtils(selector);
+
+  const element = domUtils.getElement();
+
+  if (!element) {
+    throw new Error('_() 에러: element가 null 입니다.');
+  }
+
+  // element.innerHTML = () => alert(true);
+
+  return element;
+}
+
+// type isNull = (value: any) => boolean;
+
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch() {
+    return {};
+  }
 
-	export function isNull() {}
+  /**
+   * @param {*} value
+   * @returns {boolean}
+   */
+  export function isNull(value: unknown) {
+    return value === null;
+  }
 
-	export function isNil() {}
+  /**
+   * @param {*} value
+   * @returns {boolean}
+   */
+  export function isNil(value: unknown) {
+    return value == null;
+  }
 
-	export function isNumber() {}
+  /**
+   * @param {*} value
+   * @returns {boolean}
+   */
+  export function isNumber(value: unknown) {
+    return typeof value === 'number';
+  }
 
-	export function isFunction() {}
+  /**
+   * @param {*} value
+   * @returns {boolean}
+   */
+  export function isFunction(value: unknown) {
+    return typeof value === 'function';
+  }
 
-	export function shuffle() {}
+  /**
+   * 배열의 인자의 순서를 무작위로 섞은 새로운 배열을 반환하는 함수
+   * @param {Array} array
+   * @returns {Array}
+   */
+  export function shuffle(array: []) {
+    if (!array.length) {
+      return [];
+    }
+    const shuffled = new Array(...array);
+    return shuffled.sort(() => Math.random() - 0.5);
+  }
 
-	export function pick() {}
+  /**
+   * 객체에서 키가 일치하는 프로퍼티만 포함한 새로운 객체를 반환하는 함수
+   * @param {Object} object
+   * @param {string[]} keys
+   * @returns {Object}
+   */
+  export function pick(object: Object, keys: []) {
+    const picked = {};
 
-	export function omit() {}
+    for (const key of keys) {
+      picked[key] = object[key];
+    }
 
-	export function memoize() {}
+    return picked;
+  }
 
-	export function debounce() {}
+  // 객체에서 키가 일치하는 프로퍼티를 제외한 새로운 객체를 반환하는 함수
+  export function omit(object: any, keys: any) {
+    const omitted: any = {};
 
-	export function throttle() {}
+    const pickedKeys = Object.keys(object).filter((key) => !keys.includes(key));
 
-	export function clickOutside() {}
+    for (const key of pickedKeys) {
+      omitted[key] = object[key];
+    }
+
+    return omitted;
+  }
+
+  export function memoize() {}
+
+  export function debounce() {}
+
+  export function throttle() {}
+
+  export function clickOutside() {}
 }
 
 export default _;
