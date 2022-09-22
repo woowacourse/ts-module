b4a86c9d8a5de964511311d89a40c9079686caee
Yo Wook Kim <uk960214@gmail.com>
2022-09-22 21:17:11 +0900

    chore: 기본 설정 및 필수 패키지 설치

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+};
+
+export default config;
