aef7bfa31f1006aa9dfda114db7899b15970f4fb
Yo Wook Kim <uk960214@gmail.com>
2022-09-25 01:28:09 +0900

    test: fetch를 제외한 유틸 함수 타입 및 타입 테스트 작성

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -13,5 +13,43 @@ _(".button").addEvent("click", function (event) {
   expectType<MouseEvent>(event);
 });
 
-expectType<boolean>(_.isNumber(3));
-expectType<boolean>(_.isNull(null));
+expectType<true>(_.isNull(null));
+expectType<false>(_.isNull(undefined));
+expectType<false>(_.isNull(1));
+
+expectType<true>(_.isNil(null));
+expectType<true>(_.isNil(undefined));
+expectType<false>(_.isNil(1));
+
+expectType<true>(_.isNumber(3));
+expectType<false>(_.isNumber("3"));
+
+expectType<true>(_.isFunction(() => {}));
+expectType<true>(_.isFunction(console.log));
+expectType<false>(_.isFunction("3"));
+
+const sampleObject = { a: 1, b: 2 };
+expectType<string[]>(_.shuffle(["3"]));
+expectType<number[]>(_.shuffle(sampleObject));
+
+expectType<Pick<typeof sampleObject, "a">>(_.pick(sampleObject, ["a"]));
+expectType<Pick<typeof sampleObject, "a" | "b">>(
+  _.pick(sampleObject, ["a", "b"])
+);
+
+expectType<Omit<typeof sampleObject, "a">>(_.omit(sampleObject, ["a"]));
+expectType<Omit<typeof sampleObject, "a" | "b">>(
+  _.omit(sampleObject, ["a", "b"])
+);
+
+const sampleFunc = (arg1: string, arg2: number) => `${arg1}, ${arg2}`;
+
+expectType<typeof sampleFunc>(_.memoize(sampleFunc));
+expectType<typeof sampleFunc>(_.debounce(sampleFunc, 100));
+expectType<typeof sampleFunc>(_.throttle(sampleFunc, 100));
+
+expectType<void>(
+  _.clickOutside(button, (e) => {
+    console.log(e);
+  })
+);
--- index.d.ts
+++ index.d.ts
@@ -8,8 +8,43 @@ declare global {
   function _(selector: string): Element;
 
   module _ {
-    function isNull(input: unknown): input is null;
-    function isNumber(input: unknown): input is Number;
+    function isNull<T>(input: T): T extends null ? true : false;
+    function isNil<T>(input: T): T extends null | undefined ? true : false;
+    function isNumber<T>(input: T): T extends number ? true : false;
+    function isFunction<T>(input: T): T extends Function ? true : false;
+    function isFunction<T>(input: T): T extends Function ? true : false;
+
+    function shuffle<T>(collection: T[]): T[];
+    function shuffle<U extends Record<string, unknown>>(
+      collection: U
+    ): U[keyof U][];
+
+    function pick<T extends Record<string, unknown>, U extends keyof T>(
+      object: T,
+      props?: U[]
+    ): Pick<T, U>;
+    function omit<T extends Record<string, unknown>, U extends keyof T>(
+      object: T,
+      props?: U[]
+    ): Omit<T, U>;
+
+    function memoize<T extends unknown[], U>(
+      func: (...args: T) => U,
+      resolver?: (...args: T) => string
+    ): (...args: T) => U;
+    function debounce<T extends unknown[], U>(
+      func: (...args: T) => U,
+      wait?: number
+    ): (...args: T) => U;
+    function throttle<T extends unknown[], U>(
+      func: (...args: T) => U,
+      wait?: number
+    ): (...args: T) => U;
+
+    function clickOutside(
+      targetElement: Element,
+      handler: (event: MouseEvent) => void
+    ): void;
   }
 }
 
