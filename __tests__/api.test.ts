06324125424dc4b2523548bafb93eb9d8e42ec48
kkojae <jaejeung3210@gmail.com>
2022-09-24 22:04:07 +0900

    test: _module 내보내지는지 확인하는 테스트 코드 및 _ module 안의 함수들이 잘 포함되어 있는지 확인하는 테스트 코드 작성

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,39 +1,77 @@
 /**
  * @jest-environment jsdom
  */
-import _ from '../src';
+import _ from "../src";
 
-test('모듈은 기본 내보내기', () => {
-	expect(_).toBeTruthy();
+test("모듈은 기본 내보내기", () => {
+  expect(_).toBeTruthy();
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.fetch).toBe('function');
+describe("모듈에 포함된 함수 확인", () => {
+  test("fetch 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.fetch).toBe("function");
+  });
+
+  test("isNull 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.isNull).toBe("function");
+  });
+
+  test("isNil 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.isNil).toBe("function");
+  });
+
+  test("isNumber 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.isNumber).toBe("function");
+  });
+
+  test("isFunction 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.isFunction).toBe("function");
+  });
+
+  test("shuffle 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.shuffle).toBe("function");
+  });
+
+  test("pick 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.pick).toBe("function");
+  });
+
+  test("omit 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.omit).toBe("function");
+  });
+
+  test("memoize 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.memoize).toBe("function");
+  });
+
+  test("debounce 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.debounce).toBe("function");
+  });
+
+  test("throttle 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.throttle).toBe("function");
+  });
+
+  test("clickOutside 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.clickOutside).toBe("function");
+  });
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.pick).toBe('function');
-});
+// test("Selector 동작 확인", () => {
+//   const divElement = document.createElement("div");
+//   divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+//   document.body.appendChild(divElement);
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.omit).toBe('function');
-});
+//   const buttonElement = _("button.test-btn");
+//   expect(buttonElement).toBeTruthy();
 
-test('Selector 동작 확인', () => {
-	const divElement = document.createElement('div');
-	divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
-	document.body.appendChild(divElement);
+//   document.body.removeChild(buttonElement);
+// });
 
-	const buttonElement = _('button.test-btn');
-	expect(buttonElement).toBeTruthy();
+// test('`_("").innerHTML()`~~~~', () => {});
 
-	document.body.removeChild(buttonElement);
-});
+// test('`_("").show()`~~~~', () => {});
 
-test('`_("").innerHTML()`~~~~', () => {});
+// test('`_("").hidden()`~~~~', () => {});
 
-test('`_("").show()`~~~~', () => {});
-
-test('`_("").hidden()`~~~~', () => {});
-
-test('`_("").addEvent()`~~~~', () => {});
+// test('`_("").addEvent()`~~~~', () => {});
