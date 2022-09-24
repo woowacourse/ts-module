48e486913c202e316ea005d672475d73abb4157f
hwangjungmin <hwangstar156@gmail.com>
2022-09-24 19:17:36 +0900

    feat: fetch 타입에 대한 테스트 작성

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -1,10 +1,16 @@
 /**
  * @jest-environment jsdom
  */
-import { expectType } from 'tsd';
+import { expectType } from "tsd";
 
-import _ from '../src';
+import _ from "../src";
 
-_('.button').addEvent('click', function (event) {
-	expectType<MouseEvent>(event);
+_(".button").addEvent("click", function (event) {
+  expectType<MouseEvent>(event);
 });
+
+expectType<Promise<{ name: string }>>(
+  _.fetch<{ name: string }>("https://example.com", {
+    method: "GET",
+  }).then((res) => res.json())
+);
