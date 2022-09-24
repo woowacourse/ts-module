9b128e4e7e9a4ad323b4301662988c80c006cd10
hwangjungmin <hwangstar156@gmail.com>
2022-09-24 19:27:24 +0900

    feat: isNil 타입에 대한 테스트 작성

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -17,3 +17,7 @@ expectType<Promise<{ name: string }>>(
 
 expectType<true>(_.isNull(null));
 expectType<false>(_.isNull("2"));
+
+expectType<true>(_.isNil(undefined));
+expectType<true>(_.isNil(null));
+expectType<false>(_.isNil("2"));
