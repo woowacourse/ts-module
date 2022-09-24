0861bcc8d4a41e41fa6d71c69ebcb1321bc5ae50
kkojae <jaejeung3210@gmail.com>
2022-09-24 22:05:21 +0900

    test: _ module 안의 함수들이 잘 동작하는지 확인하는 테스트 코드 작성
    - isNull, isNil, isNumber, isFunction 함수 동작 테스트 작성

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -57,6 +57,49 @@ describe("모듈에 포함된 함수 확인", () => {
   });
 });
 
+describe("isNull 함수가 정상적으로 동작하는지 확인", () => {
+  test("isNull 함수 인자에 null값을 넣을 경우 true를 return 한다.", () => {
+    expect(_.isNull(null)).toBe(true);
+  });
+
+  test("isNull 함수 인자에 null이 아닌 값을 넣을 경우 false를 return 한다.", () => {
+    expect(_.isNull(2)).toBe(false);
+  });
+});
+
+describe("isNil 함수가 정상적으로 동작하는지 확인", () => {
+  test("isNil 함수 인자에 null 또는 undefined 값을 넣을 경우 true를 return 한다.", () => {
+    expect(_.isNil(null)).toBe(true);
+    expect(_.isNil(undefined)).toBe(true);
+  });
+
+  test("isNull 함수 인자에 null 또는 undefined 가 아닌 값을 넣을 경우 false를 return 한다.", () => {
+    expect(_.isNil(2)).toBe(false);
+  });
+});
+
+describe("isNumber 함수가 정상적으로 동작하는지 확인", () => {
+  test("isNumber 함수 인자에 type이 number인 값이 들어갈 경우 true를 return 한다.", () => {
+    expect(_.isNumber(2)).toBe(true);
+  });
+
+  test("isNumber 함수 인자에 type이 number가 아닌 값이 들어갈 경우 false를 return 한다.", () => {
+    expect(_.isNumber("2")).toBe(false);
+  });
+});
+
+describe("isFunction 함수가 정상적으로 동작하는지 확인", () => {
+  test("isFunction 함수 인자에 type이 function인 값이 들어갈 경우 true를 return 한다.", () => {
+    const testFunc = () => {};
+
+    expect(_.isFunction(testFunc)).toBe(true);
+  });
+
+  test("isFunction 함수 인자에 type이 function인 값이 들어갈 경우 true를 return 한다.", () => {
+    expect(_.isFunction(2)).toBe(false);
+  });
+});
+
 // test("Selector 동작 확인", () => {
 //   const divElement = document.createElement("div");
 //   divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
