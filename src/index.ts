d4ecbcdca7ac52d13a39893f91fa9c01ded28671
KIM YEJI <rladpwl0512@gmail.com>
2022-09-26 01:59:50 +0900

    feat: debounce 타입 선언

--- src/index.ts
+++ src/index.ts
@@ -29,15 +29,32 @@ module _ {
 
   export function shuffle<T>(value: Array<T> | object): Array<T>;
 
-  // ...keys로 들어올 수도 있다. 이 부분도 해결하기! 
-  export function pick<T, K extends keyof T>(obj: T, keys?: Array<K>): Pick<T, K>;
+  // ...keys로 들어올 수도 있다. 이 부분도 해결하기!
+  export function pick<T, K extends keyof T>(
+    obj: T,
+    keys?: Array<K>
+  ): Pick<T, K>;
 
-  // ...keys로 들어올 수도 있다. 이 부분도 해결하기! 
-  export function omit<T, K extends keyof T>(obj: T, keys?: Array<K>): Omit<T, K>;
+  // ...keys로 들어올 수도 있다. 이 부분도 해결하기!
+  export function omit<T, K extends keyof T>(
+    obj: T,
+    keys?: Array<K>
+  ): Omit<T, K>;
 
   // export function memoize() {}
 
-  // export function debounce() {}
+  interface DebounceOptionsType {
+    leading?: boolean;
+    maxWait?: number;
+    trailing?: boolean;
+  }
+
+  // TODO: Function 더 구체적으로 타이핑
+  export function debounce(
+    func: Function,
+    wait?: number,
+    options?: DebounceOptionsType
+  ): Function;
 
   // export function throttle() {}
 
