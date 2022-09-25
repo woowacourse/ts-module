cf827c560274445eba0fb848681c3ba18ce3111e
woose28 <dldntmdsla@naver.com>
2022-09-26 05:09:20 +0900

    test: addEvent 타입 테스트의 전처리 및 후처리 과정 추가
    - 실제로 유틸 함수가 사용되는 상황과 유사하게 테스트 환경을 구현하기 위해 전처리, 후처리 과정을 추가

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -6,6 +6,13 @@ import { expectType, expectError } from 'tsd';
 import _ from '../src';
 
 describe('_(selector).addEvent 타입 테스트', () => {
+  const divElement = document.createElement('div');
+
+  beforeEach(() => {
+    divElement.innerHTML = `<button class='button'>Continue</button>`;
+    document.body.appendChild(divElement);
+  });
+
   test('click 이벤트를 등록할 때 listener 함수에 전달되는 이벤트 객체의 타입은 MouseEvent이다.', () => {
     _('.button').addEvent('click', function (event) {
       expectType<MouseEvent>(event);
@@ -24,5 +31,9 @@ describe('_(selector).addEvent 타입 테스트', () => {
 
   test('listener를 전달하지 않으면 에러가 발생한다.', () => {
     expectError(_('.button').addEvent('click'));
+  });
+
+  afterEach(() => {
+    document.body.removeChild(divElement);
   });
 });
