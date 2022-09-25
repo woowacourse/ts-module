8d0cf8d3cb32d197b5ae67fcbc436e371e1fb68d
woose28 <dldntmdsla@naver.com>
2022-09-25 17:16:19 +0900

    style: prettier 설정 및 적용

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -6,5 +6,5 @@ import { expectType } from 'tsd';
 import _ from '../src';
 
 _('.button').addEvent('click', function (event) {
-	expectType<MouseEvent>(event);
+  expectType<MouseEvent>(event);
 });
