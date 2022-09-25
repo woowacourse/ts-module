a32ea1b86609125849ab2c981b45a3e2307264c4
woose28 <dldntmdsla@naver.com>
2022-09-26 05:30:48 +0900

    feat: CustomElement 타입 구현 수정
    - 편리한 사용성을 위해 HTMLElement 타입을 확장해서 사용하도록 구현
    - 편리한 사용성을 위해 제네릭 타입의 조건에 HTMLElementEventMap 활용

--- src/type.ts
+++ src/type.ts
@@ -1,20 +1,18 @@
 /**
- * 이벤트 타입과 이벤트 객체를 매핑하는 객체이다.
+ * CustomElement는 Element를 확장한 타입이다.
+ * CustomElement는 다음과 같은 메서드를 추가로 갖는다.
+ *
+ * - addEvent
  */
-type MappedEventType = {
-  click: MouseEvent;
-  input: Event;
-};
-
-export type CustomElement = {
+export interface CustomElement extends HTMLElement {
   /**
    * Target element에 listener로 전달한 함수를 type 이벤트로 추가한다.
    *
    * @param type - Type은 target 요소에 등록할 이벤트 타입이다.
    * @param listener - Listener는 target 요소의 type 이벤트가 발생했을 때 호출되는 함수이다.
    */
-  addEvent: <T extends keyof MappedEventType>(
+  addEvent: <T extends keyof HTMLElementEventMap>(
     type: T,
-    listener: (event?: MappedEventType[T]) => void,
+    listener: (event?: HTMLElementEventMap[T]) => void,
   ) => void;
-};
+}
