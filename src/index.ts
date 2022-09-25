02d62184791e5d1b4cb7b933666a511c919ed8bc
JASUN LEE <liswktjs@gmail.com>
2022-09-25 15:11:16 +0900

    feat: is- 함수들 구현 및 타입 선언
    isNull, isNill, isNumber, isFunction 구현 및 타입 선언

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,64 @@
 function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+  /**
+   * innerHTML() {
+   * }
+   *
+   * show() {
+   * }
+   *
+   * hidden() {
+   * }
+   *
+   * addEvent() {
+   * }
+   */
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch() {
+    return {};
+  }
 
-	export function isNull() {}
+  export function isNull<T>(
+    arg: T
+  ): boolean {
+    return arg === null;
+  }
 
-	export function isNil() {}
+  export function isNil<T>(
+    arg: T
+  ): boolean {
+    return arg === null ||
+      arg === undefined
+      ? true
+      : false;
+  }
 
-	export function isNumber() {}
+  export function isNumber<T>(
+    arg: T
+  ): boolean {
+    return typeof arg === "number";
+  }
 
-	export function isFunction() {}
+  export function isFunction<T>(
+    arg: T
+  ): boolean {
+    return typeof arg === "function";
+  }
 
-	export function shuffle() {}
+  export function shuffle() {}
 
-	export function pick() {}
+  export function pick() {}
 
-	export function omit() {}
+  export function omit() {}
 
-	export function memoize() {}
+  export function memoize() {}
 
-	export function debounce() {}
+  export function debounce() {}
 
-	export function throttle() {}
+  export function throttle() {}
 
-	export function clickOutside() {}
+  export function clickOutside() {}
 }
 
 export default _;
