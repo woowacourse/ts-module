d90c1b1304839fbcc3bbb184da1d34c780d914fa
woose28 <dldntmdsla@naver.com>
2022-09-26 04:08:07 +0900

    test: addEvent 타입 테스트 구현

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -1,10 +1,28 @@
 /**
  * @jest-environment jsdom
  */
-import { expectType } from 'tsd';
+import { expectType, expectError } from 'tsd';
 
 import _ from '../src';
 
-_('.button').addEvent('click', function (event) {
-  expectType<MouseEvent>(event);
+describe('_(selector).addEvent 타입 테스트', () => {
+  test('click 이벤트를 등록할 때 listener 함수에 전달되는 이벤트 객체의 타입은 MouseEvent이다.', () => {
+    _('.button').addEvent('click', function (event) {
+      expectType<MouseEvent>(event);
+    });
+  });
+
+  test('input 이벤트를 등록할 때 listener 함수에 전달되는 이벤트 객체의 타입은 Event이다.', () => {
+    _('.button').addEvent('input', function (event) {
+      expectType<Event>(event);
+    });
+  });
+
+  test('유효하지 않은 이벤트를 등록할 때 타입 에러가 발생한다.', () => {
+    expectError(_('.button').addEvent('foo', function (event) {}));
+  });
+
+  test('listener를 전달하지 않으면 에러가 발생한다.', () => {
+    expectError(_('.button').addEvent('click'));
+  });
 });
