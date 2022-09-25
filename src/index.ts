d829265a38e61c8a10dc0f1de65afc480f679f53
Yo Wook Kim <uk960214@gmail.com>
2022-09-25 20:09:52 +0900

    feat: 타입 체크 매서드 구현

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,57 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+function _(selector: string) {
+  /**
+   * innerHTML() {
+   * }
+   *
+   * show() {
+   * }
+   *
+   * hidden() {
+   * }
+   *
+   * addEvent() {
+   * }
+   */
+  const element = document.querySelector(selector);
+  if (!element) throw new Error("요소 없음");
+
+  return element;
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch() {
+    return {};
+  }
 
-	export function isNull() {}
+  export function isNull(input: unknown): input is null {
+    return input === null;
+  }
 
-	export function isNil() {}
+  export function isNil(input: unknown): input is null | undefined {
+    return input === null || input === undefined;
+  }
 
-	export function isNumber() {}
+  export function isNumber(input: unknown): input is Number {
+    return typeof input === "number";
+  }
 
-	export function isFunction() {}
+  export function isFunction(input: unknown): input is Function {
+    return typeof input === "function";
+  }
 
-	export function shuffle() {}
+  export function shuffle() {}
 
-	export function pick() {}
+  export function pick() {}
 
-	export function omit() {}
+  export function omit() {}
 
-	export function memoize() {}
+  export function memoize() {}
 
-	export function debounce() {}
+  export function debounce() {}
 
-	export function throttle() {}
+  export function throttle() {}
 
-	export function clickOutside() {}
+  export function clickOutside() {}
 }
 
 export default _;
