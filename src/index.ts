9718092e5329420329b4d80d59de66b0f4a66bec
prefer2 <sunho6200@gmail.com>
2022-09-25 14:31:03 +0900

    feat: isNull 타입 추가 및 구현

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,51 @@
 function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+  /**
+   * innerHTML() {
+   * }
+   *
+   * show() {
+   * }
+   *
+   * hidden() {
+   * }
+   *
+   * addEvent() {
+   * }
+   */
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch() {
+    return {};
+  }
 
-	export function isNull() {}
+  /**
+   * @param value
+   * @returns use `is` for user-defined type guard. return true if value is null
+   */
+  export function isNull(value: unknown): value is null {
+    return value === null;
+  }
 
-	export function isNil() {}
+  export function isNil() {}
 
-	export function isNumber() {}
+  export function isNumber() {}
 
-	export function isFunction() {}
+  export function isFunction() {}
 
-	export function shuffle() {}
+  export function shuffle() {}
 
-	export function pick() {}
+  export function pick() {}
 
-	export function omit() {}
+  export function omit() {}
 
-	export function memoize() {}
+  export function memoize() {}
 
-	export function debounce() {}
+  export function debounce() {}
 
-	export function throttle() {}
+  export function throttle() {}
 
-	export function clickOutside() {}
+  export function clickOutside() {}
 }
 
 export default _;
