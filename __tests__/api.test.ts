a554a76e44d7aa8eeb2d8d4a4cd84290b2e5a955
Daye Lee <508yeah@gmail.com>
2022-09-27 06:37:12 +0900

    feat: dom 유틸 구현

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -4,30 +4,30 @@
 import _ from '../src';
 
 test('모듈은 기본 내보내기', () => {
-	expect(_).toBeTruthy();
+  expect(_).toBeTruthy();
 });
 
 test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.fetch).toBe('function');
+  expect(typeof _.fetch).toBe('function');
 });
 
 test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.pick).toBe('function');
+  expect(typeof _.pick).toBe('function');
 });
 
 test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.omit).toBe('function');
+  expect(typeof _.omit).toBe('function');
 });
 
 test('Selector 동작 확인', () => {
-	const divElement = document.createElement('div');
-	divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
-	document.body.appendChild(divElement);
+  const divElement = document.createElement('div');
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
 
-	const buttonElement = _('button.test-btn');
-	expect(buttonElement).toBeTruthy();
+  const buttonElement = _('button.test-btn').getElement();
+  expect(buttonElement).toBeTruthy();
 
-	document.body.removeChild(buttonElement);
+  divElement.removeChild(buttonElement);
 });
 
 test('`_("").innerHTML()`~~~~', () => {});
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+function _(selector: string) {
+  const element = <HTMLElement>document.querySelector(selector);
+
+  const getElement = () => element;
+
+  const innerHTML = () => element.innerHTML;
+
+  const show = () => {
+    element.style.display = '';
+  };
+
+  const hidden = () => {
+    element.style.display = 'none';
+  };
+
+  const addEvent = <K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions
+  ) => {
+    element.addEventListener(type, listener, options);
+  };
+
+  return {
+    getElement,
+    innerHTML,
+    show,
+    hidden,
+    addEvent,
+  };
 }
 
 module _ {
