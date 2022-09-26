7c8e539bd69dcd671784d4dcab43ba0cd9447764
soyi47 <soyi.dev@gmail.com>
2022-09-27 02:22:18 +0900

    test: 테스트 케이스 작성

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,39 +1,56 @@
 /**
  * @jest-environment jsdom
  */
-import _ from '../src';
+import _ from "../src";
 
-test('모듈은 기본 내보내기', () => {
-	expect(_).toBeTruthy();
+test("모듈은 기본 내보내기", () => {
+  expect(_).toBeTruthy();
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.fetch).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.fetch).toBe("function");
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.pick).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.pick).toBe("function");
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.omit).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.omit).toBe("function");
 });
 
-test('Selector 동작 확인', () => {
-	const divElement = document.createElement('div');
-	divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
-	document.body.appendChild(divElement);
+describe("Selector 및 메서드 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
+  const buttonElement = _("button.test-btn");
 
-	const buttonElement = _('button.test-btn');
-	expect(buttonElement).toBeTruthy();
+  test("Selector 동작 확인", () => {
+    expect(buttonElement).toBeTruthy();
+  });
 
-	document.body.removeChild(buttonElement);
+  test("innerHTML 동작 확인", () => {
+    expect(buttonElement.innerHTML).toBe("Continue");
+  });
+
+  test("show() 동작 확인", () => {
+    buttonElement.style.visibility = "hidden";
+    buttonElement.show();
+    expect(buttonElement.style.visibility).toBe("visible");
+  });
+
+  test("hidden() 동작 확인", () => {
+    buttonElement.style.visibility = "visible";
+    buttonElement.hide();
+    expect(buttonElement.style.visibility).toBe("hidden");
+  });
+
+  test("addEvent() 동작 확인", () => {
+    const mockEventCallback = jest.fn();
+
+    buttonElement.addEvent("click", mockEventCallback);
+    buttonElement.click();
+
+    expect(mockEventCallback).toBeCalled();
+  });
 });
-
-test('`_("").innerHTML()`~~~~', () => {});
-
-test('`_("").show()`~~~~', () => {});
-
-test('`_("").hidden()`~~~~', () => {});
-
-test('`_("").addEvent()`~~~~', () => {});
--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -1,10 +1,69 @@
 /**
  * @jest-environment jsdom
  */
-import { expectType } from 'tsd';
+import { expectType } from "tsd";
 
-import _ from '../src';
+import _ from "../src";
 
-_('.button').addEvent('click', function (event) {
-	expectType<MouseEvent>(event);
+_(".button").addEvent("click", function (event) {
+  expectType<MouseEvent>(event);
 });
+
+expectType<Promise<Response>>(_.fetch("https://test.com"));
+expectType<Promise<Response>>(
+  _.fetch("https://test.com", {
+    method: "GET",
+  })
+);
+
+expectType<boolean>(_.isNull(null));
+expectType<boolean>(_.isNull(0));
+
+expectType<boolean>(_.isNil(undefined));
+expectType<boolean>(_.isNil(NaN));
+
+expectType<number[]>(_.shuffle<number>([1, 2, 3, 4]));
+expectType<string[]>(_.shuffle<string>(["a", "b", "c"]));
+expectType<object>(_.shuffle([]));
+
+expectType<{ a: number }>(_.pick({ a: 1, b: "c" }, ["a"]));
+
+expectType<{ b: string }>(_.omit({ a: 1, b: "c" }, ["a"]));
+
+expectType<(a: number, b: number) => number>(
+  _.memoize((a: number, b: number) => {
+    return a + b;
+  })
+);
+
+expectType<(a: number, b: number) => number>(
+  _.memoize(
+    (a: number, b: number) => {
+      return a + b;
+    },
+    (a: number) => a.toString()
+  )
+);
+
+expectType<(str1: string, str2: string) => number>(
+  _.memoize((str1: string, str2: string) => str1.length + str2.length)
+);
+
+expectType<(a: number, b: number) => void>(
+  _.debounce((a: number, b: number) => {
+    console.log(a, b);
+  }, 500)
+);
+
+expectType<(a: number, b: number) => void>(
+  _.throttle((a: number, b: number) => {
+    console.log(a, b);
+  }, 500)
+);
+
+const div = document.createElement("div");
+expectType<void>(
+  _.clickOutside(div, () => {
+    console.log("click outside");
+  })
+);
