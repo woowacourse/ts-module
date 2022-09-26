dc7f2f4a3bd0ebaf902af1c1f274dedc7760ce00
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-26 17:03:47 +0900

    test: isNumber에 대한 테스트 코드 작성

---
+++ __tests__/isNumber.type.test.ts
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+import { expectType } from 'tsd';
+import _ from '../index';
+
+expectType<boolean>(_.isNumber(2));
+expectType<boolean>(_.isNumber('2'));
+expectType<boolean>(_.isNumber(true));
+expectType<boolean>(_.isNumber(null));
+expectType<boolean>(_.isNumber(undefined));
+expectType<boolean>(_.isNumber([]));
+expectType<boolean>(_.isNumber({}));
