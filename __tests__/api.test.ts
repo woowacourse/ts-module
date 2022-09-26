277987d0aecc7c9ae1303237f1cd90413eb5b12d
moonheekim0118 <hellomooneekim@gmail.com>
2022-09-26 17:26:21 +0900

    feat: HtmlElement 관련 api 테스트를 추가한다.

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,36 +1,48 @@
 /**
  * @jest-environment jsdom
  */
-import _ from '../src';
+import _ from "../src";
 
-test('모듈은 기본 내보내기', () => {
-	expect(_).toBeTruthy();
+test("모듈은 기본 내보내기", () => {
+  expect(_).toBeTruthy();
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.fetch).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.fetch).toBe("function");
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.pick).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.pick).toBe("function");
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.omit).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.omit).toBe("function");
 });
 
-test('Selector 동작 확인', () => {
-	const divElement = document.createElement('div');
-	divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
-	document.body.appendChild(divElement);
+test("Selector 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
 
-	const buttonElement = _('button.test-btn');
-	expect(buttonElement).toBeTruthy();
-
-	document.body.removeChild(buttonElement);
+  const buttonElement = _("button.test-btn").element!;
+  expect(buttonElement).toBeTruthy();
+  divElement.removeChild(buttonElement);
 });
 
-test('`_("").innerHTML()`~~~~', () => {});
+test("innerHTML 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.id = "target-div";
+  document.body.appendChild(divElement);
+
+  const myElement = _("#target-div");
+  myElement.insertHTML(`<button class='test-btn'>Continue</button>`);
+
+  const buttonElement = _("button.test-btn").element!;
+
+  expect(buttonElement).toBeTruthy();
+
+  myElement.element!.removeChild(buttonElement);
+});
 
 test('`_("").show()`~~~~', () => {});
 
