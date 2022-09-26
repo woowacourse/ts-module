05238afaf870c2cffa4dafd62a89ff9b5d4fd0aa
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 02:13:05 +0900

    feat: pick 함수 구현

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ declare module _ {
   function isNull(value: unknown): boolean;
   function isNil(value: unknown): boolean;
   function shuffle<T>(arr: T[]): T[];
+  function pick<T, P extends keyof T>(obj: T, target: P | P[]): Pick<T, P>;
 }
 
 export default _;
