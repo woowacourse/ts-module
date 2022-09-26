b6300b9c202b3ae10c284ee7821651acc319dfc1
Marco <bigdatajang@gmail.com>
2022-09-27 02:28:42 +0900

    refactor: 타입 테스트 코드 리팩터링

--- __tests__/index.test-d.ts
+++ __tests__/index.test-d.ts
@@ -2,35 +2,64 @@ import { expectType } from "tsd-lite";
 import _ from "../dist/index";
 import { DebouncedFunc, DebounceOptions, RealFunction } from "../src/utils";
 
-_(".button").addEvent("click", function (event) {
-	expectType<MouseEvent>(event);
+test("addEvent 함수의 반환 타입을 확인한다", () => {
+	_(".button").addEvent("click", function (event) {
+		expectType<MouseEvent>(event);
+	});
 });
 
-expectType<boolean>(_.isNull(null));
-expectType<boolean>(_.isNil(null));
-expectType<boolean>(_.isNumber(1));
-expectType<boolean>(_.isFunction(() => {}));
-expectType<Array<number | string>>(_.shuffle([1, "a", 3]));
-expectType<Pick<{ a: number; b: number; c: number }, "a" | "c"> | {}>(
-	_.pick({ a: 1, b: 2, c: 3 }, ["a", "c"])
-);
-expectType<Omit<{ a: number; b: number; c: number }, "a" | "c"> | {}>(
-	_.omit({ a: 1, b: 2, c: 3 }, ["a", "c"])
-);
+test("isNull 함수의 반환 타입을 확인한다", () => {
+	expectType<boolean>(_.isNull(null));
+});
 
-expectType<(value: any) => value is null>(_.isNull);
+test("isNull 함수의 호출 시그니처를 확인한다", () => {
+	expectType<(value: any) => value is null>(_.isNull);
+});
 
-type pick = <T extends object, U extends keyof T>(
-	object: T,
-	targetList: Array<U>
-) => Pick<T, U> | {};
-expectType<pick>(_.pick);
+test("isNil 함수의 반환 타입을 확인한다", () => {
+	expectType<boolean>(_.isNil(null));
+});
 
-type omit = <T extends object, K extends keyof T>(
-	object: T,
-	targetList: Array<K>
-) => Omit<T, K> | {};
-expectType<omit>(_.omit);
+test("isNumber 함수의 반환 타입을 확인한다", () => {
+	expectType<boolean>(_.isNumber(null));
+	expectType<boolean>(_.isNumber(1));
+});
+
+test("isFunction 함수의 반환 타입을 확인한다", () => {
+	expectType<boolean>(_.isFunction(() => {}));
+});
+
+test("shuffle 함수의 반환 타입을 확인한다", () => {
+	expectType<Array<number | string>>(_.shuffle([1, "a", 3]));
+});
+
+test("Pick 함수의 반환 타입을 확인한다", () => {
+	expectType<Pick<{ a: number; b: number; c: number }, "a" | "c"> | {}>(
+		_.pick({ a: 1, b: 2, c: 3 }, ["a", "c"])
+	);
+});
+
+test("pick 함수의 호출 시그니처를 확인한다", () => {
+	type pick = <T extends object, U extends keyof T>(
+		object: T,
+		targetList: Array<U>
+	) => Pick<T, U> | {};
+	expectType<pick>(_.pick);
+});
+
+test("omit 함수의 반환 타입을 확인한다", () => {
+	expectType<Omit<{ a: number; b: number; c: number }, "a" | "c"> | {}>(
+		_.omit({ a: 1, b: 2, c: 3 }, ["a", "c"])
+	);
+});
+
+test("omit 함수의 호출 시그니처를 확인한다", () => {
+	type omit = <T extends object, K extends keyof T>(
+		object: T,
+		targetList: Array<K>
+	) => Omit<T, K> | {};
+	expectType<omit>(_.omit);
+});
 
 test("debounce 함수의 반환 타입을 확인한다", () => {
 	const testFunc = () => {};
