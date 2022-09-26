9bbd94b4865292d3113a176f7d87c8ec56ba6e3f
jswith <typingjs@gmail.com>
2022-09-26 17:21:13 +0900

    test: DOM 메서드 테스트 작성

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -2,9 +2,18 @@
  * @jest-environment jsdom
  */
 import { expectType } from "tsd";
-
 import _ from "../src";
 
 _(".button").addEvent("click", function (event) {
   expectType<MouseEvent>(event);
 });
+
+expectType<void>(_(".divElement").showElement());
+
+expectType<void>(_(".divElement").hideElement());
+
+expectType<void>(_(".divElement").setInnerHTML());
+
+expectType<void>(
+  _(".divElement").setInnerHTML("안녕하세요. 문장 넣기 테스트입니다.")
+);
