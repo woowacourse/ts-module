aba6aa9b6f9a97af083812b3722941d2966ad423
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 00:38:58 +0900

    test: test: shuffle에 대한 타입 테스트 구현

---
+++ __tests__/shuffle.test-d.ts
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+import { expectType } from 'tsd';
+import _ from './../index';
+
+expectType<number[]>(_.shuffle([1, 2, 3]));
+expectType<string[]>(_.shuffle(['1', '2', '3']));
+expectType<(number | string)[]>(_.shuffle(['1', 2, '3']));
+expectType<(number | string)[]>(_.shuffle(['1', 2, '3']));
+expectType<(number | string | null)[]>(_.shuffle([null, 2, '3']));
+expectType<(string[] | number[])[]>(
+  _.shuffle([
+    [1, 2, 3],
+    ['1', '2'],
+  ])
+);
