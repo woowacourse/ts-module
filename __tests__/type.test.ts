a122c9a7110950f46b752a8ca9b3de7dac2c0f74
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-09-26 22:09:43 +0900

    test : type test 추가

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -1,10 +1,54 @@
 /**
  * @jest-environment jsdom
  */
-import { expectType } from 'tsd';
 
-import _ from '../src';
+import { expectType } from "tsd";
 
-_('.button').addEvent('click', function (event) {
-	expectType<MouseEvent>(event);
+import * as _ from "../src";
+
+test("isNull 타입 테스트", () => {
+  const result = _.isNull(null);
+  expectType<boolean>(result);
 });
+
+test("isNil 타입 테스트", () => {
+  const result = _.isNil(undefined);
+  expectType<boolean>(result);
+});
+
+test("isNumber 타입 테스트", () => {
+  const result = _.isNil(1234);
+  expectType<boolean>(result);
+});
+
+test("shuffle 타입 테스트", () => {
+  const result = _.shuffle([1, 2, 3, 4, "compy"]);
+  expectType<(number | string)[]>(result);
+});
+
+test("pick 타입 테스트", () => {
+  const testValue = { a: "🦖", b: "🦕" };
+  const result = _.pick(testValue, "a");
+
+  expectType<Record<"a", string>>(result);
+});
+
+test("omit 타입 테스트", () => {
+  const testValue = { a: "🦖", b: "🦕" };
+  const result = _.omit(testValue, "a");
+
+  expectType<Record<"b", string>>(result);
+});
+
+test("memoized 타입 테스트", () => {
+  const testFunction = _.memoize((a: string) => {
+    return a;
+  });
+
+  expectType<(a: string) => string>(testFunction);
+});
+
+/* _(".button").addEvent("click", function (event) {
+  expectType<MouseEvent>(event);
+});
+ */
