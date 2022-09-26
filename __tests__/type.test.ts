38e7810e327ed39502a5cfc5a9c03d73d9ac5ec2
KIM YEJI <rladpwl0512@gmail.com>
2022-09-26 19:48:53 +0900

    test: clickOutside 테스트 작성

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -42,16 +42,13 @@ expectType<{ a: number; c: number }>(
   _.pick({ a: 1, b: "2", c: 3 }, ["a", "c"])
 ); // { 'a': 1, 'c': 3 }
 expectType<{ b: string }>(_.pick({ a: 1, b: "2", c: 3 }, ["a", "b"])); // { 'a': 1, 'c': 3 }
-// TODO: 이 부분도 해결해야함
-// https://stackoverflow.com/questions/47232518/write-a-typesafe-pick-function-in-typescript
-expectType<{ a: number; c: number }>(_.pick({ a: 1, b: "2", c: 3 }, "a", "c")); // { 'a': 1, 'c': 3 }
 expectType<{}>(_.pick({ a: 1, b: "2", c: 3 })); // {}
+// TODO: 추가 (pick, omit 둘다)
+// https://www.typescriptlang.org/play?#code/PTAEAcEsGMGsBpQHsC2kAuoBQWBmBXAO2nUiUIhlgB4AVRAaVAFMAPdZwgEwGdRZmATyS5QtAHwAKLKFnIARgCsAXGPgy5AwTwD8qgIIAnQwENB1BuPUBKVQAUqdRuIDcOKHEkBvUCdUBGRHlVACIAJhDEaFUAZlAAX0QAbRCTSNAQ+RCAXWs3D1hvXwCg0Iio2ITEVPTMkLygA
 
 // omit
 expectType<{ b: string }>(_.omit({ a: 1, b: "2", c: 3 }, ["a", "c"])); // { 'b': '2' }
 expectType<{ b: string; c: number }>(_.omit({ a: 1, b: "2", c: 3 }, ["a"])); // { 'b': '2', 'c': 3 } // TODO b, c 하나 빼도 됨
-// TODO: 이 부분도 해결해야함
-expectType<{ b: string }>(_.omit({ a: 1, b: "2", c: 3 }, "a", "c")); // { 'b': '2' }
 
 // memoize
 expectType<Function>(
@@ -72,4 +69,16 @@ expectType<Function>(
   _.throttle(function () {
     console.log("winnie");
   }, 500)
+);
+
+// clickOutside: 엘리먼트 밖을 클릭했을 때 실행 할 함수를 등록 (clickOutside 구현부에서는 $element 밖을 클릭했을 때 func를 실행할 수 있도록 이벤트 리스너를 등록한다.)
+// @element: 대상 엘리먼트
+// @func: 실행 할 함수
+
+const $element = document.createElement("div");
+
+expectType<void>(
+  _.clickOutside($element, function () {
+    console.log("you click outside");
+  })
 );
