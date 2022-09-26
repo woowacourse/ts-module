89060bdb0e618b86f2919099e002b9cd97b83e13
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-09-27 00:39:47 +0900

    test : debounce 타입 테스트 추가

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -2,51 +2,57 @@
  * @jest-environment jsdom
  */
 
-import { expectType } from "tsd";
+import { expectType } from 'tsd'
 
-import * as _ from "../src";
+import * as _ from '../src'
 
-test("isNull 타입 테스트", () => {
-  const result = _.isNull(null);
-  expectType<boolean>(result);
-});
+test('isNull 타입 테스트', () => {
+  const result = _.isNull(null)
+  expectType<boolean>(result)
+})
 
-test("isNil 타입 테스트", () => {
-  const result = _.isNil(undefined);
-  expectType<boolean>(result);
-});
+test('isNil 타입 테스트', () => {
+  const result = _.isNil(undefined)
+  expectType<boolean>(result)
+})
 
-test("isNumber 타입 테스트", () => {
-  const result = _.isNil(1234);
-  expectType<boolean>(result);
-});
+test('isNumber 타입 테스트', () => {
+  const result = _.isNil(1234)
+  expectType<boolean>(result)
+})
 
-test("shuffle 타입 테스트", () => {
-  const result = _.shuffle([1, 2, 3, 4, "compy"]);
-  expectType<(number | string)[]>(result);
-});
+test('shuffle 타입 테스트', () => {
+  const result = _.shuffle([1, 2, 3, 4, 'compy'])
+  expectType<(number | string)[]>(result)
+})
 
-test("pick 타입 테스트", () => {
-  const testValue = { a: "🦖", b: "🦕" };
-  const result = _.pick(testValue, "a");
+test('pick 타입 테스트', () => {
+  const testValue = { a: '🦖', b: '🦕' }
+  const result = _.pick(testValue, 'a')
 
-  expectType<Record<"a", string>>(result);
-});
+  expectType<Record<'a', string>>(result)
+})
 
-test("omit 타입 테스트", () => {
-  const testValue = { a: "🦖", b: "🦕" };
-  const result = _.omit(testValue, "a");
+test('omit 타입 테스트', () => {
+  const testValue = { a: '🦖', b: '🦕' }
+  const result = _.omit(testValue, 'a')
 
-  expectType<Record<"b", string>>(result);
-});
+  expectType<Record<'b', string>>(result)
+})
 
-test("memoized 타입 테스트", () => {
+test('memoized 타입 테스트', () => {
   const testFunction = _.memoize((a: string) => {
-    return a;
-  });
+    return a
+  })
 
-  expectType<(a: string) => string>(testFunction);
-});
+  expectType<(a: string) => string>(testFunction)
+})
+
+test('debounce 타입 테스트', () => {
+  const testDebounced = _.debounce((text: string) => console.log(`2초 후 표시: ${text}`), 2000)
+
+  expectType<(text: string) => void>(testDebounced)
+})
 
 /* _(".button").addEvent("click", function (event) {
   expectType<MouseEvent>(event);
