e9bbdd9da0b86d60d4197344efac4a47c4ea8172
hwangjungmin <hwangstar156@gmail.com>
2022-09-27 02:16:08 +0900

    chore: tsd 테스트에 대한 환경 설정

--- __tests__/type.test-d.ts
+++ __tests__/type.test-d.ts
@@ -5,8 +5,8 @@ import { expectType } from "tsd";
 
 import _ from "../src";
 
-expectType<Promise<{ name: string }>>(
-  _.fetch<{ name: string }>("https://example.com", {
+expectType<Promise<string>>(
+  _.fetch<string>("https://example.com", {
     method: "GET",
   }).then((res) => res.json())
 );
@@ -27,16 +27,8 @@ expectType<boolean>(_.isFunction("2"));
 expectType<(1 | 2 | 3 | 4)[]>(_.shuffle([4, 2, 3, 1]));
 
 expectType<{ x: number; y: number }>(_.pick({ x: 1, y: 2, z: 3 }, ["x", "y"]));
-expectType<{ x: number; y: number }>(
-  //@ts-expect-error
-  _.pick({ x: 1, y: 2, z: 3 }, ["t", "y"])
-);
 
 expectType<{ z: number }>(_.omit({ x: 1, y: 2, z: 3 }, ["x", "y"]));
-expectType<{ x: number; y: number }>(
-  //@ts-expect-error
-  _.omit({ x: 1, y: 2, z: 3 }, ["t", "y"])
-);
 
 expectType<(a: number, b: number) => number>(
   _.memoize((a: number, b: number) => {
@@ -78,17 +70,19 @@ expectType<void>(
 
 const $Button = _(".button");
 
-$Button.addEvent("click", function (event) {
-  expectType<MouseEvent>(event);
-});
+if ($Button) {
+  $Button.addEvent("click", function (event) {
+    expectType<MouseEvent>(event);
+  });
 
-//@ts-expect-error
-$Button.addEvent("test", function (event) {
-  console.log(event);
-});
+  //@ts-expect-error
+  $Button.addEvent("test", function (event) {
+    console.log(event);
+  });
 
-expectType<void>($Button.setShow());
+  expectType<void>($Button.setShow());
 
-expectType<void>($Button.setHidden());
+  expectType<void>($Button.setHidden());
 
-expectType<void>($Button.setInnerHTML("<div>안녕하세요</div>"));
+  expectType<void>($Button.setInnerHTML("<div>안녕하세요</div>"));
+}
