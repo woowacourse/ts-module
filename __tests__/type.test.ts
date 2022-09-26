474d30521a1c83caf6409c1caff6252c9ba0ed8a
sanaandmomo <qajjang46@naver.com>
2022-09-26 20:33:37 +0900

    test: fetch 타입 테스트 작성

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -6,5 +6,11 @@ import { expectType } from 'tsd';
 import _ from '../src';
 
 _('.button').addEvent('click', function (event) {
-	expectType<MouseEvent>(event);
+  expectType<MouseEvent>(event);
 });
+
+expectType<Promise<{ name: string }>>(
+  _.fetch<{ name: string }>('https://example.com', {
+    method: 'GET',
+  }).then((res) => res.json())
+);
