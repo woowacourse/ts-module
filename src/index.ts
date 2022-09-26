9e4bb66f6bdfb2e2406574eec23dc2cfc42eef09
jswith <typingjs@gmail.com>
2022-09-26 17:20:56 +0900

    feat: DOM 메서드 타입 작성

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,42 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+declare function _(selector: string): HTMLElement;
+declare global {
+  interface HTMLElement {
+    setInnerHTML(str?: string): void;
+    showElement(): void;
+    hideElement(): void;
+    addEvent<T extends HTMLElementEventMap, K extends keyof T>(
+      type: K,
+      listener: (event: T[K]) => void
+    ): void;
+  }
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch() {
+    return {};
+  }
 
-	export function isNull() {}
+  export function isNull() {}
 
-	export function isNil() {}
+  export function isNil() {}
 
-	export function isNumber() {}
+  export function isNumber() {}
 
-	export function isFunction() {}
+  export function isFunction() {}
 
-	export function shuffle() {}
+  export function shuffle() {}
 
-	export function pick() {}
+  export function pick() {}
 
-	export function omit() {}
+  export function omit() {}
 
-	export function memoize() {}
+  export function memoize() {}
 
-	export function debounce() {}
+  export function debounce() {}
 
-	export function throttle() {}
+  export function throttle() {}
 
-	export function clickOutside() {}
+  export function clickOutside() {}
 }
 
 export default _;
