0e6959e975f77c9a935de345bcba3c5858b3725d
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-09-26 16:52:37 +0900

    feat: 유틸 함수 및 메서드 타입 선언

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,69 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+class Tt {
+  selector = "";
+  element: Element | null;
+  constructor(selector: string) {
+    this.selector = selector;
+    this.element = document.querySelector(selector);
+  }
+
+  innerHTML(innerStr: string): void {}
+  show(): void {}
+  hidden(): void {}
+  addEvent<K extends keyof GlobalEventHandlersEventMap>(
+    eventName: K,
+    listener: (event: GlobalEventHandlersEventMap[K]) => void
+  ): void {}
 }
 
-module _ {
-	export function fetch() {
-		return {};
-	}
-
-	export function isNull() {}
-
-	export function isNil() {}
-
-	export function isNumber() {}
-
-	export function isFunction() {}
-
-	export function shuffle() {}
-
-	export function pick() {}
-
-	export function omit() {}
-
-	export function memoize() {}
-
-	export function debounce() {}
-
-	export function throttle() {}
-
-	export function clickOutside() {}
+function tt(selector: string) {
+  return new Tt(selector);
 }
 
-export default _;
+module tt {
+  export function fetch(url: string): Promise<Response> {}
+
+  export function isNull(value: unknown): value is null {}
+
+  // null or undefined
+  export function isNil(value: unknown): value is null | undefined {}
+
+  export function isNumber(value: unknown): value is number {}
+
+  export function isFunction(
+    value: unknown
+  ): value is (...args: any[]) => any {}
+
+  export function shuffle<T>(array: Array<T>): Array<T> {}
+
+  export function pick<Obj extends Object, Key extends keyof Obj>(
+    obj: Obj,
+    keys: Array<Key>
+  ): Pick<Obj, Key> {}
+
+  export function omit<Obj extends Object, Key extends keyof Obj>(
+    obj: Obj,
+    keys: Array<Key>
+  ): Omit<Obj, Key> {}
+
+  // Creates a function that memoizes the result of func
+  export function memoize<ReturnValue>(
+    func: (...args: any) => ReturnValue
+  ): () => ReturnValue {}
+
+  export function debounce<Params extends any[], ReturnValue>(
+    func: (...params: Params) => ReturnValue,
+    timeInterval: number
+  ): (...params: Params) => ReturnValue {}
+
+  export function throttle<Params extends any[], ReturnValue>(
+    func: (...params: Params) => ReturnValue,
+    timeInterval: number
+  ): (...params: Params) => ReturnValue {}
+
+  export function clickOutside(
+    target: EventTarget,
+    handler: EventListener
+  ): void {}
+}
+
+export default tt;
