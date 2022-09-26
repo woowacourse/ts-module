391edd4ac5c4d775d40faeacad891b49a4dd23ac
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 01:39:28 +0900

    test: pick에 대한 타입 테스트 구현

---
+++ __tests__/pick.test-d.ts
@@ -0,0 +1,24 @@
+import { expectType } from 'tsd';
+import _ from './../index';
+
+const obj = {
+  name: 'al-bur',
+  age: 29,
+  job: 'frontend',
+  hobby: 'music',
+};
+
+expectType<{ name: string }>(_.pick(obj, 'name'));
+expectType<{ age: number }>(_.pick(obj, 'age'));
+expectType<{ job: string }>(_.pick(obj, 'job'));
+expectType<{ name: string; age: number }>(_.pick(obj, ['name', 'age']));
+
+const readOnlyObj = {
+  name: 'al-bur',
+  age: 29,
+  job: 'frontend',
+  hobby: 'music',
+} as const;
+
+expectType<{ job: 'frontend' }>(_.pick(readOnlyObj, 'job'));
+expectType<{ name: 'al-bur'; age: 29 }>(_.pick(readOnlyObj, ['name', 'age']));
