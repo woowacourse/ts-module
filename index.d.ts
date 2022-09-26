a814c363df42da75035d9ed6fdf9676ee9041fcb
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 00:48:53 +0900

    feat: feat: shuffle에 대한 타입 구현

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ declare module _ {
   function isNumber(value: unknown): boolean;
   function isNull(value: unknown): boolean;
   function isNil(value: unknown): boolean;
+  function shuffle<T>(arr: T[]): T[];
 }
 
 export default _;
