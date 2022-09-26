c052449bdd925479488f387e1679d671df8c73e0
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 01:30:37 +0900

    test: shuffle 함수에 대한 테스트 작성

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,39 +1,47 @@
-/**
- * @jest-environment jsdom
- */
 import _ from '../src';
 
 test('모듈은 기본 내보내기', () => {
-	expect(_).toBeTruthy();
+  expect(_).toBeTruthy();
 });
 
 test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.fetch).toBe('function');
+  expect(typeof _.fetch).toBe('function');
 });
 
 test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.pick).toBe('function');
+  expect(typeof _.pick).toBe('function');
 });
 
 test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.omit).toBe('function');
+  expect(typeof _.omit).toBe('function');
 });
 
-test('Selector 동작 확인', () => {
-	const divElement = document.createElement('div');
-	divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
-	document.body.appendChild(divElement);
+test('shuffle 함수에 숫자로된 배열을 넣으면 요소들의 순서가 바뀐 배열이 반환된다', () => {
+  const arr = [1, 2, 3];
+  const shuffledArr = _.shuffle(arr);
 
-	const buttonElement = _('button.test-btn');
-	expect(buttonElement).toBeTruthy();
-
-	document.body.removeChild(buttonElement);
+  expect(
+    arr[0] === shuffledArr[0] &&
+      arr[1] === shuffledArr[1] &&
+      arr[2] === shuffledArr[2]
+  ).toBe(false);
 });
 
-test('`_("").innerHTML()`~~~~', () => {});
+// test('Selector 동작 확인', () => {
+//   const divElement = document.createElement('div');
+//   divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+//   document.body.appendChild(divElement);
 
-test('`_("").show()`~~~~', () => {});
+//   const buttonElement = _('button.test-btn');
+//   expect(buttonElement).toBeTruthy();
 
-test('`_("").hidden()`~~~~', () => {});
+//   document.body.removeChild(buttonElement);
+// });
 
-test('`_("").addEvent()`~~~~', () => {});
+// test('`_("").innerHTML()`~~~~', () => {});
+
+// test('`_("").show()`~~~~', () => {});
+
+// test('`_("").hidden()`~~~~', () => {});
+
+// test('`_("").addEvent()`~~~~', () => {});
