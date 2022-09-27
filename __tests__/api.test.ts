1ada22cb4780d0f1fc73174a6d1a4163bf220db2
junzzi <jjh01027717502@gmail.com>
2022-09-27 14:40:45 +0900

    feat: 기능 구현 및 API 테스트 완료

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,39 +1,143 @@
 /**
  * @jest-environment jsdom
  */
-import _ from '../src';
+import _ from "../src";
 
-test('모듈은 기본 내보내기', () => {
-	expect(_).toBeTruthy();
+test("모듈은 기본 내보내기", () => {
+  expect(_).toBeTruthy();
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.fetch).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.fetch).toBe("function");
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.pick).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.pick).toBe("function");
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.omit).toBe('function');
+test("모듈에 포함된 함수 확인", () => {
+  expect(typeof _.omit).toBe("function");
 });
 
-test('Selector 동작 확인', () => {
-	const divElement = document.createElement('div');
-	divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
-	document.body.appendChild(divElement);
+test("Selector 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
 
-	const buttonElement = _('button.test-btn');
-	expect(buttonElement).toBeTruthy();
+  const buttonElement = _("button.test-btn").get();
+  expect(buttonElement).toBeTruthy();
 
-	document.body.removeChild(buttonElement);
+  divElement.removeChild(buttonElement);
 });
 
-test('`_("").innerHTML()`~~~~', () => {});
+test('`_("").innerHTML()`~~~~', () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
 
-test('`_("").show()`~~~~', () => {});
+  const buttonElementObject = _("button.test-btn");
 
-test('`_("").hidden()`~~~~', () => {});
+  buttonElementObject.innerHTML('<div id="test-div"></div>');
 
-test('`_("").addEvent()`~~~~', () => {});
+  const testDivElement = _("#test-div");
+  expect(testDivElement).toBeTruthy();
+});
+
+test('`_("").show()`~~~~', () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn' style="display:none">Continue</button>`;
+  document.body.appendChild(divElement);
+
+  const buttonElementObject = _("button.test-btn");
+  const buttonElement = _("button.test-btn").get();
+
+  buttonElementObject.show();
+
+  expect(buttonElement.style.display).toBe("");
+});
+
+test('`_("").hidden()`~~~~', () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn' style="display:""">Continue</button>`;
+  document.body.appendChild(divElement);
+
+  const buttonElementObject = _("button.test-btn");
+  const buttonElement = _("button.test-btn").get();
+
+  buttonElementObject.hidden();
+
+  expect(buttonElement.style.display).toBe("none");
+});
+
+test('`_("").addEvent()`~~~~', () => {
+  const clickEventHandler = jest.fn();
+
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
+
+  const buttonElementObject = _("button.test-btn");
+  const buttonElement = buttonElementObject.get();
+  buttonElementObject.addEvent("click", clickEventHandler);
+
+  buttonElement.click();
+  expect(clickEventHandler).toBeCalledTimes(1);
+});
+
+test("_.pick 동작 테스트", () => {
+  const condition = {
+    a: 10,
+    b: 10,
+  };
+
+  const result = _.pick(condition, "a");
+
+  expect(result["b"]).toBe(undefined);
+});
+
+test("_.omit 동작 테스트", () => {
+  const condition = {
+    a: 10,
+    b: 10,
+  };
+
+  const result = _.omit(condition, "a");
+
+  expect(result["a"]).toBe(undefined);
+});
+
+test("debounce 함수 확인", () => {
+  const mockedFunction = jest.fn();
+  const debouncedFunction = _.debounce(mockedFunction, 100);
+
+  for (let i = 0; i < 1000; i++) {
+    debouncedFunction();
+    jest.advanceTimersByTime(50);
+  }
+
+  jest.runAllTimers();
+  expect(mockedFunction).toHaveBeenCalledTimes(1);
+});
+
+test("throttle 함수 확인", () => {
+  const mockedFunction: jest.Mock = jest.fn();
+  const throttledFunction = _.throttle(mockedFunction, 1000);
+
+  for (let i = 0; i < 1000; i++) {
+    throttledFunction();
+    jest.advanceTimersByTime(10);
+  }
+
+  expect(mockedFunction).toHaveBeenCalledTimes(10);
+});
+
+test("memoize 함수 확인", () => {
+  const memoizedFunction = _.memoize(() => {
+    return { a: 10, b: 10 };
+  });
+
+  const firstCallResult = memoizedFunction();
+  const secondCallResult = memoizedFunction();
+
+  expect(firstCallResult === secondCallResult).toBeTruthy();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,174 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+function keys<T extends Record<string, unknown>, U extends keyof T>(
+  object: T
+): U[] {
+  const keys: unknown = Object.keys(object);
+  return keys as U[];
+}
+
+function _(selector: string): {
+  get: () => HTMLElement;
+  innerHTML: (template: string) => void;
+  show: () => void;
+  hidden: () => void;
+  addEvent: (
+    type: keyof HTMLElementEventMap,
+    handler: (this: Element, ev: Event) => void
+  ) => void;
+} {
+  const element = document.querySelector(selector);
+
+  if (element === null) {
+    throw new Error("엘리먼트를 찾지 못하였습니다");
+  }
+
+  return {
+    get() {
+      return element as HTMLElement;
+    },
+    innerHTML(template) {
+      element.innerHTML = template;
+    },
+    show() {
+      (element as HTMLElement).style.display = "";
+    },
+    hidden() {
+      (element as HTMLElement).style.display = "none";
+    },
+    addEvent(type, handler) {
+      element.addEventListener(type, handler);
+    },
+  };
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export async function fetch<T>(
+    input: URL | RequestInfo,
+    init?: RequestInit | undefined
+  ): Promise<T> {
+    const response = await window.fetch(input, init);
 
-	export function isNull() {}
+    if (!response.ok) {
+      throw new Error("에러");
+    }
 
-	export function isNil() {}
+    const json: unknown = await response.json();
 
-	export function isNumber() {}
+    return json as T;
+  }
 
-	export function isFunction() {}
+  export function isNull(arg: unknown): arg is null {
+    return arg === null;
+  }
 
-	export function shuffle() {}
+  export function isNil(arg: unknown): arg is null | undefined {
+    return arg == null;
+  }
 
-	export function pick() {}
+  export function isNumber(arg: unknown): arg is number {
+    return typeof arg === "number";
+  }
 
-	export function omit() {}
+  export function isFunction(arg: unknown): arg is (...args: any[]) => any {
+    return typeof arg === "function";
+  }
 
-	export function memoize() {}
+  export function shuffle<T>(
+    list: T[] | null | undefined
+  ): T[] | null | undefined {
+    return list?.sort(() => Math.random() - 0.5);
+  }
 
-	export function debounce() {}
+  export function pick<T extends Record<string, unknown>, U extends keyof T>(
+    object: T,
+    ...props: Array<U>
+  ): Pick<T, U> {
+    return props.reduce<Pick<T, U>>(
+      (prev, currentKey) => ({
+        ...prev,
+        [currentKey]: object[currentKey],
+      }),
+      {} as Pick<T, U>
+    );
+  }
 
-	export function throttle() {}
+  export function omit<T extends Record<string, unknown>, U extends keyof T>(
+    object: T,
+    ...props: Array<U>
+  ): Omit<T, U> {
+    return keys<typeof object, U>(object).reduce<Omit<T, U>>(
+      (prev, currentKey) => {
+        if (props.includes(currentKey)) {
+          return prev;
+        }
+        return { ...prev, [currentKey]: object[currentKey] };
+      },
+      {} as Omit<T, U>
+    );
+  }
 
-	export function clickOutside() {}
+  export function memoize(callbackFunction: Function): Function {
+    const cache: {
+      memoized: boolean;
+      data: any;
+    } = {
+      memoized: false,
+      data: null,
+    };
+
+    const memoizedFunction = (props: any) => {
+      if (!cache.memoized) {
+        cache.memoized = true;
+        cache.data = callbackFunction(props);
+      }
+
+      return cache.data;
+    };
+
+    return memoizedFunction;
+  }
+
+  export function debounce(callbackFunction: Function, time: number): Function {
+    const debounce: {
+      timeout: NodeJS.Timeout | null;
+    } = {
+      timeout: null,
+    };
+
+    const debouncedFunction = (props: any) => {
+      if (debounce.timeout) {
+        clearTimeout(debounce.timeout);
+      }
+
+      debounce.timeout = setTimeout(() => {
+        callbackFunction(props);
+        debounce.timeout = null;
+      }, time);
+    };
+
+    return debouncedFunction;
+  }
+
+  export function throttle(callbackFunction: Function, time: number): Function {
+    const throttle: {
+      timeout: NodeJS.Timeout | null;
+    } = {
+      timeout: null,
+    };
+
+    const throttledFunction = (props: any) => {
+      if (throttle.timeout) {
+        return;
+      }
+
+      throttle.timeout = setTimeout(() => {
+        callbackFunction(props);
+        throttle.timeout = null;
+      }, time);
+    };
+
+    return throttledFunction;
+  }
+
+  export function clickOutside() {}
 }
-
 export default _;
