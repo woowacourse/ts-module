44c1d74badfa5ebd40f8e9170237a764dad18707
intae92 <kangit2013@gmail.com>
2022-09-27 22:03:45 +0900

    test: type test

--- __tests__/tsd/type.test-d.ts
+++ __tests__/tsd/type.test-d.ts
@@ -5,11 +5,11 @@ import { expectType } from "tsd";
 
 import _ from "../../src";
 
-// _(".button").addEvent("click", function (event: any) {
-//   expectType<MouseEvent>(event);
-// });
-
 expectType<boolean>(_.isNull(undefined));
-// expectType<boolean>(_.isNull("string"));
+expectType<boolean>(_.isNull("string"));
 
-// /** @type {import('ts-jest').JestConfigWithTsJest} */
+expectType<{ a: "123" }>(_.pick({ a: "123", b: "bb" }, "a"));
+expectType<{ a: "123"; b: "bb" }>(
+  _.pick({ a: "123", b: "bb", c: 1 }, "a", "b")
+);
+expectType<{ a: 1 }>(_.pick({ a: 1, b: "bb" }, "a"));
