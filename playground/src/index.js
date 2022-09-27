9b20625bab084e8784db87a64b90bb8f3fc877f4
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-09-27 20:56:48 +0900

    chore : playground 수정

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,12 +1,25 @@
 import './styles/global'
-import * as local from '../../dist'
-import * as publish from '@compy-ryu/ts-module'
-import $ from './utils'
-
-console.log(local.isNull(null))
+import $local from '../../dist'
+import $ from '@compy-ryu/ts-module'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const debounced = publish.debounce(() => alert('클릭 후 1초 후에 출력합니다'), 1000)
+  console.log($.isNull(null))
 
-  $('#test').addEventListener('click', debounced)
+  const debounced = $.debounce(() => alert('클릭 후 1초 후에 출력합니다'), 1000)
+
+  $('#debounce').addEvent('click', debounced)
+  $('#fetch').addEvent('click', async () => {
+    const result = await $.fetch('http://localhost:9000', {
+      headers: {
+        Authorization: 'Bearer test-header',
+      },
+    })
+
+    const data = await result.json()
+
+    console.log(data)
+
+    const testElement = $('#fetch')
+    document.body.removeChild(testElement)
+  })
 })
