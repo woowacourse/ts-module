a71f4ac0768c74b46b538a80d73d18cef68a4bf0
airman5573 <rpf5573@kakao.com>
2022-09-27 14:46:20 +0900

    feat: TQuery 구현

--- src/TQuery.ts
+++ src/TQuery.ts
@@ -3,31 +3,54 @@ import { TQueryType } from "./types/index";
 class TQuery implements TQueryType {
   els: NodeListOf<HTMLElement> | null = null;
   get length() {
-    return 0;
+    if (this.els === null) return 0;
+    return this.els.length;
   }
 
   constructor(selector: string) {
     const elements = document.querySelectorAll<HTMLElement>(selector);
-    if (!elements) return;
+    if (elements === null) return;
     this.els = elements;
   }
 
-  loop = (callback: (el: HTMLElement) => void) => () => {};
+  loop = (callback: (el: HTMLElement) => void) => () => {
+    if (this.els === null) return;
+    this.els.forEach((element: HTMLElement) => {
+      callback(element);
+    });
+  };
 
   [Symbol.iterator]() {
+    const elements = this.els ?? [];
+    let count = 0;
     return {
       next: () => {
-        return { value: new HTMLElement(), done: true };
+        if (elements.length === 0) {
+          return {
+            done: true,
+          };
+        }
+        if (count === 0) {
+          count += 1;
+          return {
+            value: elements[0],
+            done: elements.length === count,
+          };
+        }
+        return {
+          value: elements[count++],
+          done: elements.length === count,
+        };
       },
     };
   }
 
   show() {
-    return "show";
+    return this.loop((el) => (el.hidden = false));
   }
 
   hidden() {
-    return "hidden";
+    return this.loop((el) => (el.hidden = true));
   }
 
   addEvent<K extends keyof HTMLElementEventMap>(
@@ -35,11 +58,11 @@ class TQuery implements TQueryType {
     listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
     options?: boolean | AddEventListenerOptions
   ) {
-    return "addEvent";
+    return this.loop((el) => el.addEventListener(type, listener, options))();
   }
 
   get(index: number) {
-    if (!this.els) return null;
+    if (this.els === null) return null;
     return this.els[index];
   }
 }
