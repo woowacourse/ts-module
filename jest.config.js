104a0fc988022c63b014ce9f826bac5f0bc4306c
이아현 <lah1203@naver.com>
2022-09-27 22:47:38 +0900

    fix: 정규식 물음표 제거

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   testRegex: '/__tests__/api.test\\.ts$',
