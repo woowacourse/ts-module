c197ccc0df3f15c05c3d87b0dd3f9de7e0e9f575
Ahn <tn3748@naver.com>
2022-09-27 22:49:11 +0900

    feat: test 구현

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -1,24 +1,24 @@
 import { expectType } from "tsd";
 import _ from "../src";
 
-// _(".button").addEvent("click", function (event) {
-//   expectType<MouseEvent>(event);
-// });
+_(".button").addEvent("click", function (event) {
+  expectType<MouseEvent>(event);
+});
 
-// expectType<boolean>(_.isNull(null));
-// expectType<boolean>(_.isNull("ahn"));
+expectType<boolean>(_.isNull(null));
+expectType<boolean>(_.isNull("ahn"));
 
-// expectType<boolean>(_.isNil(null));
-// expectType<boolean>(_.isNil(undefined));
-// expectType<boolean>(_.isNil("ahn"));
+expectType<boolean>(_.isNil(null));
+expectType<boolean>(_.isNil(undefined));
+expectType<boolean>(_.isNil("ahn"));
 
-// expectType<boolean>(_.isNumber(123));
-// expectType<boolean>(_.isNumber("ahn"));
+expectType<boolean>(_.isNumber(123));
+expectType<boolean>(_.isNumber("ahn"));
 
-// expectType<boolean>(_.isFunction(() => {}));
-// expectType<boolean>(_.isFunction("ahn"));
+expectType<boolean>(_.isFunction(() => {}));
+expectType<boolean>(_.isFunction("ahn"));
 
-// expectType<(1 | 2 | 3 | 4)[]>(_.shuffle([1, 2, 3, 4]));
+expectType<(1 | 2 | 3 | 4)[]>(_.shuffle([1, 2, 3, 4]));
 
 test("isNill 함수에 대한 타입을 체크한다.", () => {
   expectType<(value: any) => boolean>(_.isNil);
