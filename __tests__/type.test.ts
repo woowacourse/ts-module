bf6127837eceba33750c41f2ed5aedef57ce0a58
kwannee <yleh1126@naver.com>
2022-09-27 22:59:01 +0900

    test: 테스트 코드 작성
    - 진행 중

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -1,10 +1,18 @@
 /**
  * @jest-environment jsdom
  */
-import { expectType } from 'tsd';
+import { expectType } from "tsd";
 
-import _ from '../src';
+import _ from "../src/index";
 
-_('.button').addEvent('click', function (event) {
-	expectType<MouseEvent>(event);
-});
+const object = { a: 1, b: 2, c: 3 };
+
+expectType<boolean>(_.isNull(null));
+expectType<boolean>(_.isNil(undefined));
+expectType<boolean>(_.isFunction(() => {}));
+expectType<number[]>(_.shuffle([1, 2, 3, 4]));
+expectType<Pick<typeof object, "a" | "c">>(_.pick(object, ["a", "c"]));
+expectType<Omit<typeof object, "a" | "c"> | {}>(_.omit(object, ["a", "c"]));
+expectType<(eventTarget: HTMLElement, innerElement: HTMLElement) => boolean>(
+  _.clickOutside
+);
