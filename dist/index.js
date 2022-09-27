40be34e2c6b05902b92279a67ff019199ebbb234
woose28 <dldntmdsla@naver.com>
2022-09-28 06:28:54 +0900

    docs: 유틸 함수 설명 추가

--- dist/index.js
+++ dist/index.js
@@ -15,35 +15,31 @@ exports.__esModule = true;
  * 전달한 selector에 해당되는 요소를 찾고, 해당 요소에서 사용할 수 있는 커스텀 메서드를 반환한다.
  *
  * @param selector - Selector은 document의 자식 요소들 중 selector와 일치하는 요소를 찾을 때 사용된다.
- * @returns HTMLElement를 확장한 CustomElement 객체 또는 null을 반환한다..
+ * @returns HTMLElement를 확장한 CustomElement 객체 또는 null을 반환한다.
  */
 function _(selector) {
     var target = document.querySelector(selector);
     if (target !== null) {
+        target.insertHTML = function (html) {
+            target.innerHTML = html;
+            return target.innerHTML;
+        };
         target.addEvent = function (type, listener) {
             target.addEventListener(type, listener);
         };
+        target.show = function () {
+            target.style.display = 'block';
+        };
+        target.hide = function () {
+            target.style.display = 'none';
+        };
     }
     return target;
-    /**
-     * innerHTML() {
-     * }
-     *
-     * show() {
-     * }
-     *
-     * hidden() {
-     * }
-     *
-     * addEvent() {
-     * }
-     */
 }
 (function (_) {
-    function fetch() {
-        return {};
-    }
-    _.fetch = fetch;
+    _.fetch = function (url, options) {
+        return window.fetch(url, options);
+    };
     /**
      * 전달한 value가 null인지 확인하는 함수
      *
@@ -74,12 +70,25 @@ function _(selector) {
      * @returns Value가 함수이면 true를 반환한다. 그렇지 않으면 false를 반환한다.
      */
     _.isFunction = function (value) { return value instanceof Function; };
+    /**
+     * 전달한 collection을 값 또는 요소를 무작위로 섞은 후 결과를 반환한다.
+     *
+     * @param collection - Collection은 요소를 섞을 배열 또는 값을 섞을 객체이다.
+     * @returns Collection은 요소 또는 값을 섞은 결과를 배열로 반환한다..
+     */
     _.shuffle = function (collection) {
         if (collection instanceof Array) {
             return collection.sort(function () { return (Math.random() > 0.5 ? 1 : -1); });
         }
         return Object.values(collection).sort(function () { return (Math.random() > 0.5 ? 1 : -1); });
     };
+    /**
+     * 전달한 object의 키 중 paths에 속한 키들만을 찾아서 반환한다.
+     *
+     * @param object - 대상이 되는 object이다.
+     * @param paths - Object의 키들 중 반환할 키들의 배열이다.
+     * @returns Object의 키 중 paths에 속한 키들을 찾고, 새로운 객체를 만들어서 반환한다.
+     */
     _.pick = function (object, paths) {
         var copiedObject1 = __assign({}, object);
         var copiedObject2 = __assign({}, object);
@@ -94,6 +103,13 @@ function _(selector) {
         });
         return copiedObject2;
     };
+    /**
+     * 전달한 object의 키 중 paths에 속한 키들만을 제외하고 반환한다.
+     *
+     * @param object - 대상이 되는 object이다.
+     * @param paths - Object의 키들 중 제외할 키들의 배열이다.
+     * @returns Object의 키 중 paths에 속한 키들을 제외하고, 새로운 객체를 만들어서 반환한다.
+     */
     _.omit = function (object, paths) {
         var copiedObject = __assign({}, object);
         paths.forEach(function (key) {
@@ -101,13 +117,86 @@ function _(selector) {
         });
         return copiedObject;
     };
-    function memoize() { }
-    _.memoize = memoize;
-    function debounce() { }
-    _.debounce = debounce;
-    function throttle() { }
-    _.throttle = throttle;
-    function clickOutside() { }
-    _.clickOutside = clickOutside;
+    /**
+     * Func로 전달한 함수의 실행 결과를 캐싱하고, 캐싱 된 결괏값을 반환하는 함수이다.
+     *
+     * @param func - Func는 실행 결괏값을 캐싱 할 함수이다.
+     * @returns Func의 실행 결과를 반환한다. 단, 이미 캐싱 된 결괏값이 존재한다면 func 함수를 실행하지 않고 캐싱 된 값을 반환한다.
+     */
+    _.memoize = function (func) {
+        var cache = {};
+        return function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            var key = JSON.stringify(args);
+            if (!cache.hasOwnProperty(key)) {
+                cache[key] = func.apply(this, args);
+            }
+            return cache[key];
+        };
+    };
+    /**
+     * Func로 전달한 함수의 실행을 debouncing을 적용해서 제어하는 함수이다.
+     *
+     * @param func - Func는 debouncing을 적용할 함수이다.
+     * @param wait - Wait은 debouncing을 처리할 시간이다.(ms)
+     * @return 익명 함수를 반환한다. 해당 함수를 실행하면 func로 전달한 함수가 debouncing이 적용돼서 실행된다.
+     */
+    _.debounce = function (func, wait) {
+        if (wait === void 0) { wait = 0; }
+        var timerId = null;
+        return function () {
+            var _this = this;
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            if (timerId) {
+                clearTimeout(timerId);
+            }
+            timerId = window.setTimeout(function () {
+                timerId = null;
+                return func.apply(_this.args);
+            }, wait);
+        };
+    };
+    /**
+     * Func로 전달한 함수의 실행을 throttling을 적용해서 제어하는 함수이다.
+     *
+     * @param func - Func는 throttling을 적용할 함수이다.
+     * @param wait - Wait은 throttling 처리할 시간이다.(ms)
+     * @return 익명 함수를 반환한다. 해당 함수를 실행하면 func로 전달한 함수가 throttling이 적용돼서 실행된다.
+     */
+    _.throttle = function (func, wait) {
+        if (wait === void 0) { wait = 0; }
+        var timerId = null;
+        return function () {
+            var _this = this;
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            if (timerId) {
+                timerId = window.setTimeout(function () {
+                    func.apply(_this, args);
+                    timerId = null;
+                }, wait);
+            }
+        };
+    };
+    /**
+     * Element로 전달한 요소의 부묘 요소에 click 이벤트를 등록하는 메서드이다.
+     *
+     * @param element - Element는 대상이 되는 요소이다. 해당 요소의 부모 요소에 click 이벤트를 등록한다.
+     * @param callback - Callback은 Element의 부모 요소 click 이벤트에 등록할 메서드이다.
+     */
+    _.clickOutside = function (element, callback) {
+        var parentElement = element.parentElement;
+        parentElement.addEventListener('click', function (event) {
+            callback(event);
+        });
+    };
 })(_ || (_ = {}));
 exports["default"] = _;
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,18 @@
  * CustomElement는 HTMLElement를 확장한 타입이다.
  * CustomElement는 다음과 같은 메서드를 추가로 갖는다.
  *
+ * - insertHTML
  * - addEvent
+ * - show
+ * - hide
  */
 interface CustomElement extends HTMLElement {
+  /**
+   * Target 요소에 html로 전달한 문자열을 innerHTML로 추가한 후 결과륿 반환한다.
+   *
+   * @param html - HTML은 target 요소에 삽입할 html 문서이다.
+   * @return 변경된 내부 html를 반환한다.
+   */
   insertHTML: (html: string) => string;
   /**
    * Target element에 listener로 전달한 함수를 type 이벤트로 추가한다.
@@ -16,7 +25,13 @@ interface CustomElement extends HTMLElement {
     type: T,
     listener: (event: HTMLElementEventMap[T]) => void,
   ) => void;
+  /**
+   * Target 요소의 style.display 속성을 block으로 변경한다.
+   */
   show: () => void;
+  /**
+   * Target 요소의 style.display 속성을 none으로 변경한다.
+   */
   hide: () => void;
 }
 
@@ -112,6 +127,12 @@ namespace _ {
    */
   export const isFunction: TypeValidator = (value) => value instanceof Function;
 
+  /**
+   * 전달한 collection을 값 또는 요소를 무작위로 섞은 후 결과를 반환한다.
+   *
+   * @param collection - Collection은 요소를 섞을 배열 또는 값을 섞을 객체이다.
+   * @returns Collection은 요소 또는 값을 섞은 결과를 배열로 반환한다..
+   */
   export const shuffle: Shuffle = (collection) => {
     if (collection instanceof Array) {
       return collection.sort(() => (Math.random() > 0.5 ? 1 : -1));
@@ -120,6 +141,13 @@ namespace _ {
     return Object.values(collection).sort(() => (Math.random() > 0.5 ? 1 : -1));
   };
 
+  /**
+   * 전달한 object의 키 중 paths에 속한 키들만을 찾아서 반환한다.
+   *
+   * @param object - 대상이 되는 object이다.
+   * @param paths - Object의 키들 중 반환할 키들의 배열이다.
+   * @returns Object의 키 중 paths에 속한 키들을 찾고, 새로운 객체를 만들어서 반환한다.
+   */
   export const pick: Pick = (object, paths) => {
     const copiedObject1 = { ...object };
     const copiedObject2 = { ...object };
@@ -139,6 +167,13 @@ namespace _ {
     return copiedObject2;
   };
 
+  /**
+   * 전달한 object의 키 중 paths에 속한 키들만을 제외하고 반환한다.
+   *
+   * @param object - 대상이 되는 object이다.
+   * @param paths - Object의 키들 중 제외할 키들의 배열이다.
+   * @returns Object의 키 중 paths에 속한 키들을 제외하고, 새로운 객체를 만들어서 반환한다.
+   */
   export const omit: Omit = (object, paths) => {
     const copiedObject = { ...object };
 
@@ -149,6 +184,12 @@ namespace _ {
     return copiedObject;
   };
 
+  /**
+   * Func로 전달한 함수의 실행 결과를 캐싱하고, 캐싱 된 결괏값을 반환하는 함수이다.
+   *
+   * @param func - Func는 실행 결괏값을 캐싱 할 함수이다.
+   * @returns Func의 실행 결과를 반환한다. 단, 이미 캐싱 된 결괏값이 존재한다면 func 함수를 실행하지 않고 캐싱 된 값을 반환한다.
+   */
   export const memoize = <T extends Function, U>(
     func: T,
   ): ((...args: unknown[]) => U) => {
@@ -165,6 +206,13 @@ namespace _ {
     };
   };
 
+  /**
+   * Func로 전달한 함수의 실행을 debouncing을 적용해서 제어하는 함수이다.
+   *
+   * @param func - Func는 debouncing을 적용할 함수이다.
+   * @param wait - Wait은 debouncing을 처리할 시간이다.(ms)
+   * @return 익명 함수를 반환한다. 해당 함수를 실행하면 func로 전달한 함수가 debouncing이 적용돼서 실행된다.
+   */
   export const debounce = <T extends Function>(func: T, wait = 0) => {
     let timerId: number | null = null;
 
@@ -180,6 +228,13 @@ namespace _ {
     };
   };
 
+  /**
+   * Func로 전달한 함수의 실행을 throttling을 적용해서 제어하는 함수이다.
+   *
+   * @param func - Func는 throttling을 적용할 함수이다.
+   * @param wait - Wait은 throttling 처리할 시간이다.(ms)
+   * @return 익명 함수를 반환한다. 해당 함수를 실행하면 func로 전달한 함수가 throttling이 적용돼서 실행된다.
+   */
   export const throttle = <T extends Function>(func: T, wait = 0) => {
     let timerId: number | null = null;
 
@@ -193,6 +248,12 @@ namespace _ {
     };
   };
 
+  /**
+   * Element로 전달한 요소의 부묘 요소에 click 이벤트를 등록하는 메서드이다.
+   *
+   * @param element - Element는 대상이 되는 요소이다. 해당 요소의 부모 요소에 click 이벤트를 등록한다.
+   * @param callback - Callback은 Element의 부모 요소 click 이벤트에 등록할 메서드이다.
+   */
   export const clickOutside = <T extends HTMLElement, U extends Function>(
     element: T,
     callback: U,
