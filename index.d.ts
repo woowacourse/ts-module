bf500ba72ab62d6d077a091785fab4f9c1dc5437
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 17:15:33 +0900

    feat: clickOutside 함수의 타입 작성

--- index.d.ts
+++ index.d.ts
@@ -8,6 +8,7 @@ declare namespace _ {
   function memoize(func: Function): Function;
   function debounce(func: Function, delay: number): Function;
   function throttle(func: Function, delay: number): Function;
+  function clickOutside(element: HTMLElement, func: Function): void;
 }
 
 export default _;
