425d5d0922fbc4054c43a13d3d9e2d9bfe0cd36c
airman5573 <rpf5573@kakao.com>
2022-09-27 20:36:51 +0900

    feat: fetch 함수 추가

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,3 +1,6 @@
-const fetch = () => {};
+// node v18 부터 native fetch를 지원합니다
+const fetch = (input: RequestInfo | URL, init?: RequestInit) => {
+  return globalThis.fetch(input, init);
+};
 
 export default fetch;
