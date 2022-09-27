04a1b29957451e29ea63ffdb56a6f9d152b46fc0
cks3066 <cks3066@naver.com>
2022-09-27 22:43:50 +0900

    feat: throttle 구현

---
+++ src/libs/throttle.ts
@@ -0,0 +1,21 @@
+/**
+ * throttle
+ *
+ * @param {Function} callback
+ *
+ * @param {number} delay
+ *
+ * @returns {void}
+ */
+export function throttle(callback: Function, delay: number) {
+  let timer: NodeJS.Timeout | null = null;
+
+  return function () {
+    if (!timer) {
+      timer = setTimeout(function (this: unknown) {
+        timer = null;
+        callback.apply(this, arguments);
+      }, delay);
+    }
+  };
+}
