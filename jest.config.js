8a66efca242575f8d60fcf8a9d3dd35145a6295d
intae92 <kangit2013@gmail.com>
2022-09-27 21:59:39 +0900

    chore: 환경 설정 수정

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: "ts-jest",
   moduleFileExtensions: ["js", "ts"],
   testEnvironment: "jsdom",
-  testMatch: ["<rootDir>/__tests__/*.ts"],
+  testMatch: ["<rootDir>/__tests__/*test.ts"],
 };
