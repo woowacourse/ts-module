2f139245612d7a7c85c8ec613c1e43aaffe3fa48
jswith <typingjs@gmail.com>
2022-09-27 21:18:01 +0900

    test: type 테스트 수정

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -16,21 +16,21 @@ expectType<void>(
   )
 );
 
-expectType<Promise<Response>>(_.fetch("https://github.com/jswith"));
+expectType<Promise<string>>(_.fetch("https://github.com/jswith"));
 
-expectType<true>(_.isNull(null));
-expectType<false>(_.isNull(3));
+expectType<boolean>(_.isNull(null));
+expectType<boolean>(_.isNull(3));
 
-expectType<true>(_.isNil(null));
-expectType<true>(_.isNil(undefined));
-expectType<false>(_.isNil(NaN));
+expectType<boolean>(_.isNil(null));
+expectType<boolean>(_.isNil(undefined));
+expectType<boolean>(_.isNil(NaN));
 
-expectType<true>(_.isNumber(2));
-expectType<true>(_.isNumber(NaN));
-expectType<false>(_.isNumber("3"));
+expectType<boolean>(_.isNumber(2));
+expectType<boolean>(_.isNumber(NaN));
+expectType<boolean>(_.isNumber("3"));
 
-expectType<true>(_.isFunction((a: number, b: number) => a + b));
-expectType<false>(_.isFunction({ a: 1, b: 2 }));
+expectType<boolean>(_.isFunction((a: number, b: number) => a + b));
+expectType<boolean>(_.isFunction({ a: 1, b: 2 }));
 
 expectType<Array<string | number>>(_.shuffle([1, "a", 3, 4]));
 expectType<Array<string | number | true>>(_.shuffle(["a", "b", 3, true]));
@@ -41,6 +41,7 @@ expectType<Record<string | number, unknown>>(
 expectType<Record<string | number, unknown>>(
   _.pick({ a: 1, b: 2, c: "3", d: "f" }, ["a", "c"])
 );
+fetch;
 
 expectType<Record<string | number, unknown>>(
   _.omit({ a: 1, b: 2, c: "g" }, ["a"])
@@ -49,10 +50,18 @@ expectType<Record<string | number, unknown>>(
   _.omit({ a: 1, b: 2, c: "3", d: "f" }, ["a", "c"])
 );
 
-expectType<unknown>(_.memoize((a: number, b: number) => a + b));
+expectType<(...args: any) => number>(
+  _.memoize((a: number, b: number) => a + b)
+);
 
-expectType<Function>(_.debounce((a: number, b: number) => a + b, 400));
+expectType<(...args: any) => void>(
+  _.debounce((a: number, b: number) => a + b, 400)
+);
 
-expectType<Function>(_.throttle((a: number, b: number) => a + b, 300));
+expectType<(...args: any) => void>(
+  _.throttle((a: number, b: number) => a + b, 300)
+);
 
-expectType<void>(_.clickOutside(_(".button") as HTMLElement));
+expectType<void>(
+  _.clickOutside(_(".button") as HTMLElement, (a: number, b: number) => a + b)
+);
