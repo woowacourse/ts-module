fe3e4f85095693555f004c614713965a56b53f92
KIM YEJI <rladpwl0512@gmail.com>
2022-09-27 22:28:30 +0900

    feat: index.d.ts 빌드

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,25 @@
-declare function _(selector: string): any;
+declare global {
+    interface HTMLElement extends CustomElement {
+    }
+}
+interface CustomElement {
+    insertHTML: (value: string) => void;
+    hide: () => void;
+    show: () => void;
+    addEvent: <K extends keyof HTMLElementEventMap>(type: K, listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any) => void;
+}
+declare function _(selector: string): HTMLElement | undefined;
 declare module _ {
     function fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
     function isNull<T>(value: T): T extends null ? true : false;
     function isNil<T>(value: T): T extends null | undefined ? true : false;
     function isNumber<T>(value: T): T extends number ? true : false;
     function isFunction<T>(value: T): T extends Function ? true : false;
-    function shuffle<T>(value: Array<T> | object): Array<T>;
-    function pick<T, K extends keyof T>(obj: T, keys?: Array<K>): Pick<T, K>;
-    function omit<T, K extends keyof T>(obj: T, keys?: Array<K>): Omit<T, K>;
-    function memoize(func: Function, resolver?: Function): Function;
-    interface DebounceOptionsType {
-        leading?: boolean;
-        maxWait?: number;
-        trailing?: boolean;
-    }
-    function debounce(func: Function, wait?: number, options?: DebounceOptionsType): Function;
-    interface ThrottleOptionsType {
-        leading?: boolean;
-        trailing?: boolean;
-    }
-    function throttle(func: Function, wait?: number, options?: ThrottleOptionsType): Function;
+    function shuffle<T>(value: Array<T>): Array<T>;
+    function pick<T extends Record<string, unknown>, K extends keyof T>(obj: T, keys?: Array<K>): Pick<T, K> | {};
+    function omit<T extends Record<string, unknown>, K extends keyof T>(obj: T, keys?: Array<K>): Omit<T, K>;
+    function debounce<Params extends unknown[]>(func: (...args: Params) => unknown, wait: number): (...args: Params) => unknown;
+    function throttle<Params extends unknown[]>(func: (...args: Params) => unknown, wait: number): (...args: Params) => unknown;
     function clickOutside(elem: HTMLElement, func: Function): void;
 }
 export default _;
