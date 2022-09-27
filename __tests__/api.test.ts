cfec1f8b45090a4a4c531599e858b88f8f8d98ff
kimsanglok <evertree6031@gmail.com>
2022-09-27 18:50:52 +0900

    test: 모듈에 포함된 함수 확인 테스트 작성

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -3,3 +3,15 @@ import _ from "../src";
 test("모듈은 기본 내보내기를 테스트한다.", () => {
   expect(_).toBeTruthy();
 });
+
+test("모듈에 포함된 함수를 확인한다.", () => {
+  expect(typeof _.fetch).toBe("function");
+});
+
+test("모듈에 포함된 함수를 확인한다.", () => {
+  expect(typeof _.pick).toBe("function");
+});
+
+test("모듈에 포함된 함수를 확인한다.", () => {
+  expect(typeof _.omit).toBe("function");
+});
