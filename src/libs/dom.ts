34831f888c84e2537fdc7ed03cf72fd00732e821
cks3066 <cks3066@naver.com>
2022-09-27 22:44:25 +0900

    feat: dom 구현

---
+++ src/libs/dom.ts
@@ -0,0 +1,49 @@
+class Dom {
+  #element: HTMLElement | null = null;
+
+  constructor(selector: string) {
+    this.#element = document.querySelector(selector);
+  }
+
+  get() {
+    if (!this.#element) {
+      throw new Error('innerHTML 에러: element가 null 입니다.');
+    }
+
+    return this.#element;
+  }
+
+  innerHTML() {
+    if (!this.#element) {
+      throw new Error('innerHTML 에러: element가 null 입니다.');
+    }
+    return this.#element.innerHTML;
+  }
+
+  show() {
+    if (!this.#element) {
+      throw new Error('innerHTML 에러: element가 null 입니다.');
+    }
+    this.#element.style.visibility = 'visible';
+  }
+
+  hidden() {
+    if (!this.#element) {
+      throw new Error('innerHTML 에러: element가 null 입니다.');
+    }
+    this.#element.style.visibility = 'hidden';
+  }
+
+  addEvent(
+    event: string,
+    listener: (this: Element, ev: Event) => any,
+    options?: boolean | AddEventListenerOptions | undefined
+  ) {
+    if (!this.#element) {
+      throw new Error('addEvent 에러: element가 null 입니다.');
+    }
+    this.#element.addEventListener(event, listener, options);
+  }
+}
+
+export default Dom;
