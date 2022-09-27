babb8f06f1bf8d559ab24320f1c98589982677a9
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 09:11:34 +0900

    test: pick 함수에 대한 테스트 코드 구현

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -17,14 +17,29 @@ test('모듈에 포함된 함수 확인', () => {
 });
 
 test('shuffle 함수에 숫자로된 배열을 넣으면 요소들의 순서가 바뀐 배열이 반환된다', () => {
-  const arr = [1, 2, 3];
+  const arr = [1, 2, 3, 4, 5];
   const shuffledArr = _.shuffle(arr);
 
   expect(
     arr[0] === shuffledArr[0] &&
       arr[1] === shuffledArr[1] &&
-      arr[2] === shuffledArr[2]
+      arr[2] === shuffledArr[2] &&
+      arr[3] === shuffledArr[3] &&
+      arr[4] === shuffledArr[4]
   ).toBe(false);
+});
+
+test('pick 함수로 원하는 요소만 따로 추출할 수 있다.', () => {
+  const obj = {
+    name: 'al-bur',
+    age: 29,
+  };
+
+  expect(_.pick(obj, 'name')).toStrictEqual({ name: 'al-bur' });
+  expect(_.pick(obj, ['name', 'age'])).toStrictEqual({
+    name: 'al-bur',
+    age: 29,
+  });
 });
 
 // test('Selector 동작 확인', () => {
