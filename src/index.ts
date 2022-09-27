076c7c162e881e55252f76977f75328810739f2f
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 09:33:40 +0900

    commit: omit 함수 구현

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ type isNullType = typeof __.isNull;
 type isNilType = typeof __.isNil;
 type shuffleType = typeof __.shuffle;
 type PickType = typeof __.pick;
+type OmitType = typeof __.omit;
 
 function _(selector: string): any {
   /**
@@ -47,19 +48,31 @@ module _ {
   export const pick: PickType = (obj, targets) => {
     // TODO: any말고 targets에 대한 타입들이 들어가야한다...
     // NOTE: as로 단언해준 이유는, 무조건 pick 함수의 반환 타입이 들어가는 게 확실하기 때문에 해주었습니다.
-    const copiedObj = {} as Pick<typeof obj, any>;
+    const pickedObj = {} as Pick<typeof obj, any>;
     // NOTE: targets인자에 'target'나 ['target1', 'target2']가 올 수 있기 때문에 분기처리 해주었습니다.
-    if (typeof targets === 'string') copiedObj[targets] = obj[targets];
-    if (targets instanceof Array) {
+    if (typeof targets === 'string') pickedObj[targets] = obj[targets];
+    if (targets instanceof Object) {
       targets.forEach((target) => {
-        copiedObj[target] = obj[target];
+        pickedObj[target] = obj[target];
       });
     }
 
-    return copiedObj;
+    return pickedObj;
   };
 
-  export function omit() {}
+  export const omit: OmitType = (obj, targets) => {
+    const omittedObj = { ...obj };
+    if (typeof targets === 'string') {
+      delete omittedObj[targets];
+    }
+    if (targets instanceof Array) {
+      targets.forEach((target) => {
+        delete omittedObj[target];
+      });
+    }
+
+    return omittedObj;
+  };
 
   export function memoize() {}
 
