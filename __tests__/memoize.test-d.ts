b32bf24b5a06d0b07c27e6f014e40446b09cd2a1
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 14:12:58 +0900

    refactor: memoize 함수에 대한 타입 수정

--- __tests__/memoize.test-d.ts
+++ __tests__/memoize.test-d.ts
@@ -1,9 +1,6 @@
 import { expectType } from 'tsd';
 import _ from './../index';
 
-expectType<Function>(
-  _.memoize(
-    () => {},
-    () => {}
-  )
-);
+// NOTE: 함수들을 인자로 받는 함수들은 그 함수들이 특정한 형식의 함수가 아니면 타입 테스트를 작성하는 게 무의미해보입니다.
+// 단순히 Function 타입을 선언해주기 때문이죠..
+expectType<Function>(_.memoize(() => {}));
--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-declare module _ {
+declare namespace _ {
   function isNumber(value: unknown): boolean;
   function isNull(value: unknown): boolean;
   function isNil(value: unknown): boolean;
   function shuffle<T>(arr: T[]): T[];
   function pick<T, P extends keyof T>(obj: T, target: P | P[]): Pick<T, P>;
   function omit<T, P extends keyof T>(obj: T, target: P | P[]): Omit<T, P>;
-  function memoize(func: Function, resolver: Function): Function;
+  function memoize(func: Function): Function;
 }
 
 export default _;
