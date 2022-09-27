36c3447b4ce58a5c1dbfb4969df610d1145dca75
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 09:11:15 +0900

    fix: pick 함수 버그 수정

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ type isNumberType = typeof __.isNumber;
 type isNullType = typeof __.isNull;
 type isNilType = typeof __.isNil;
 type shuffleType = typeof __.shuffle;
+type PickType = typeof __.pick;
 
 function _(selector: string): any {
   /**
@@ -43,7 +44,20 @@ module _ {
     return [...arr].sort(() => 0.5 - Math.random());
   };
 
-  export function pick() {}
+  export const pick: PickType = (obj, targets) => {
+    // TODO: any말고 targets에 대한 타입들이 들어가야한다...
+    // NOTE: as로 단언해준 이유는, 무조건 pick 함수의 반환 타입이 들어가는 게 확실하기 때문에 해주었습니다.
+    const copiedObj = {} as Pick<typeof obj, any>;
+    // NOTE: targets인자에 'target'나 ['target1', 'target2']가 올 수 있기 때문에 분기처리 해주었습니다.
+    if (typeof targets === 'string') copiedObj[targets] = obj[targets];
+    if (targets instanceof Array) {
+      targets.forEach((target) => {
+        copiedObj[target] = obj[target];
+      });
+    }
+
+    return copiedObj;
+  };
 
   export function omit() {}
 
