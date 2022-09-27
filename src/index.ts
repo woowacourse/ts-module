925e4387b62674b5260b944b4aadcd32d887b3e5
kkojae <jaejeung3210@gmail.com>
2022-09-27 20:50:43 +0900

    feat: util 함수 및 class 메서드 구현

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,201 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+class _ {
+  selector: string;
+
+  constructor(selector: string) {
+    this.selector = selector;
+  }
+
+  get() {
+    return document.querySelector(this.selector) as HTMLElement;
+  }
+
+  innerHTML(element: HTMLElement, text: string) {
+    element.innerHTML = text;
+  }
+
+  show(element: HTMLElement) {
+    element.style.display = "";
+  }
+
+  hidden(element: HTMLElement) {
+    element.style.display = "none";
+  }
+
+  addEvent(
+    element: HTMLElement,
+    eventType: keyof HTMLElementEventMap,
+    callback: () => void
+  ) {
+    element.addEventListener(eventType, callback);
+  }
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch(
+    path: RequestInfo | URL,
+    config?: RequestInit
+  ): Promise<Response> {
+    return window
+      .fetch(path, config)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
 
-	export function isNull() {}
+        return response.json();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
-	export function isNil() {}
+  export function isNull(value: unknown): boolean {
+    return value === null;
+  }
 
-	export function isNumber() {}
+  export function isNil(value: unknown): boolean {
+    return typeof value === "undefined" || value === null;
+  }
 
-	export function isFunction() {}
+  export function isNumber(value: unknown): boolean {
+    return typeof value === "number";
+  }
 
-	export function shuffle() {}
+  export function isFunction(value: unknown): boolean {
+    return typeof value === "function";
+  }
 
-	export function pick() {}
+  export function shuffle<T>(array: T[]) {
+    return array.sort(() => Math.random() - 0.5);
+  }
 
-	export function omit() {}
+  export function pick<T, P>(
+    obj: Record<string | symbol | number, T> | T[],
+    rest: P[]
+  ): Record<string | symbol | number, T> | T[] {
+    if (Array.isArray(obj)) {
+      const pickArray: T[] = [];
 
-	export function memoize() {}
+      rest.forEach((restItem) => {
+        if (typeof restItem !== "number") return [];
 
-	export function debounce() {}
+        pickArray.push(obj.filter((objItem) => objItem === obj[restItem])[0]);
+      });
 
-	export function throttle() {}
+      return pickArray;
+    }
 
-	export function clickOutside() {}
+    const pickObject: Record<string | symbol | number, T> = {};
+    const objEntires = Object.entries(obj);
+
+    rest.forEach((restItem) => {
+      const [key, value] = objEntires.filter(
+        (objEntry) => objEntry[0] === restItem
+      )[0];
+
+      pickObject[key] = value;
+    });
+
+    return pickObject;
+  }
+
+  export function omit<T, P>(
+    obj: Record<string | symbol | number, T> | T[],
+    rest: P[]
+  ): Record<string | symbol | number, T> | T[] {
+    if (Array.isArray(obj)) {
+      const omitArray: T[] = [...obj];
+      let idx = 0;
+
+      while (rest.length > idx) {
+        const targetItem = rest[idx];
+        if (typeof targetItem !== "number") return [];
+
+        omitArray.splice(omitArray.indexOf(obj[targetItem]), 1);
+
+        idx++;
+      }
+
+      return omitArray;
+    }
+
+    const omitObject: Record<string | symbol | number, T> = {};
+    const objEntires = Object.entries(obj);
+    let idx = 0;
+
+    while (rest.length > idx) {
+      const targetItem = rest[idx];
+      objEntires.splice(
+        objEntires.findIndex((objEntry) => objEntry[0] === targetItem),
+        1
+      );
+
+      idx++;
+    }
+
+    objEntires.forEach((objEntry) => {
+      const [key, value] = objEntry;
+      omitObject[key] = value;
+    });
+
+    return omitObject;
+  }
+
+  export function memoize<T>(func: () => T): () => T {
+    if (typeof func !== "function") {
+      throw Error("첫 번째 인자는 function을 넣어주어야 합니다.");
+    }
+
+    const cache: { memoizedData: T | null } = {
+      memoizedData: null,
+    };
+
+    return function () {
+      if (cache.memoizedData) {
+        return cache.memoizedData;
+      }
+
+      cache.memoizedData = func();
+      return cache.memoizedData;
+    };
+  }
+
+  export function debounce(callback: () => void, delay: number): () => void {
+    let timerId: NodeJS.Timeout;
+
+    return function () {
+      if (timerId) clearTimeout(timerId);
+
+      timerId = setTimeout(callback, delay);
+    };
+  }
+
+  export function throttle(callback: () => void, delay: number): () => void {
+    let timerId: NodeJS.Timeout | null;
+
+    return function () {
+      if (timerId) return;
+
+      timerId = setTimeout(() => {
+        callback();
+        timerId = null;
+      }, delay);
+    };
+  }
+
+  export function clickOutside(
+    parentElement: HTMLElement,
+    targetElement: HTMLElement,
+    callback: (targetElement: HTMLElement) => void
+  ): void {
+    parentElement.addEventListener("click", (event: MouseEvent) => {
+      if (!targetElement.contains(event.target as HTMLElement)) {
+        callback(targetElement);
+
+        return;
+      }
+    });
+  }
 }
 
 export default _;
