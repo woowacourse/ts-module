deade9b3e03bb58f6fc67c6c6949190fe17dfb78
cks3066 <cks3066@naver.com>
2022-09-27 22:43:23 +0900

    feat: memoize 구현

---
+++ src/libs/memoize.ts
@@ -0,0 +1,30 @@
+import { isNull } from './isNull';
+
+/**
+ * memoize
+ *
+ * @param {Function} callback
+ *
+ * @returns {*}
+ */
+export function memoize<T extends (...args: any[]) => unknown>(callback: T) {
+  const cache: { data: unknown | null; args: any[] } = {
+    data: null,
+    args: [],
+  };
+
+  return (...args: Parameters<T>) => {
+    if (
+      !isNull(cache.data) &&
+      args.length === cache.args.length &&
+      args.every((arg, index) => arg === cache.args[index])
+    ) {
+      return cache.data;
+    }
+
+    const result = callback(...args);
+    cache.data = result;
+    cache.args = args;
+    return result;
+  };
+}
