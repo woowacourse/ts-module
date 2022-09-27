0d7ad13aefcda1c5a9521da0ce2294510cc070cf
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-27 09:52:03 +0900

    feat: memoize 함수에 대한 타입 구현

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@ declare module _ {
   function shuffle<T>(arr: T[]): T[];
   function pick<T, P extends keyof T>(obj: T, target: P | P[]): Pick<T, P>;
   function omit<T, P extends keyof T>(obj: T, target: P | P[]): Omit<T, P>;
+  function memoize(func: Function, resolver: Function): Function;
 }
 
 export default _;
