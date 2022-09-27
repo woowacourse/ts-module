dde91663658dd11ce3c7fa643e39b1d16d29bb30
kimsanglok <evertree6031@gmail.com>
2022-09-27 18:44:15 +0900

    test: show 함수 타입 테스트 작성

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -5,3 +5,7 @@ import _ from "../src";
 test("innerHTML 함수 타입을 테스트한다. ", () => {
   expectType<string>(_("button").innerHtml("hello world"));
 });
+
+test("show 함수 타입을 테스트한다. ", () => {
+  expectType<void>(_("button").show());
+});
