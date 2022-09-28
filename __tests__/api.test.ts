44bdf78be98da3603ba9b62d0e24e239b89b5cc1
Jaejeung Ko <jaejeung3210@gmail.com>
2022-09-28 19:23:34 +0900

    [TypeScript & Module Typing] 꼬재(고재증) 미션 제출합니다. (#11)
    * chore: 기본 환경 설정
    
    - .gitignore, babel, tsconfig, package.json 설정
    
    * test: _module 내보내지는지 확인하는 테스트 코드 및 _ module 안의 함수들이 잘 포함되어 있는지 확인하는 테스트 코드 작성
    
    * test: _ module 안의 함수들이 잘 동작하는지 확인하는 테스트 코드 작성
    
    - isNull, isNil, isNumber, isFunction 함수 동작 테스트 작성
    
    * chore: tsconfig 불필요한 주석 제거
    
    * test: api test 작성
    
    * test: type test 작성
    
    * feat: util 함수 및 class 메서드 구현
    
    * refactor: type 재설정
    
    - is 사용자 정의 타입 가드를 통해 타입 네로잉
    - debounce, throttle callback 함수 인자 받을 수 있도록 type 재설정
    
    * chore: babel config 제거 -> jest config 설정

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,39 +1,265 @@
 /**
  * @jest-environment jsdom
  */
-import _ from '../src';
+import _ from "../src";
 
-test('모듈은 기본 내보내기', () => {
-	expect(_).toBeTruthy();
+jest.useFakeTimers();
+
+test("모듈은 기본 내보내기", () => {
+  expect(_).toBeTruthy();
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.fetch).toBe('function');
+describe("모듈에 포함된 함수 확인", () => {
+  test("fetch 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.fetch).toBe("function");
+  });
+
+  test("isNull 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.isNull).toBe("function");
+  });
+
+  test("isNil 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.isNil).toBe("function");
+  });
+
+  test("isNumber 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.isNumber).toBe("function");
+  });
+
+  test("isFunction 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.isFunction).toBe("function");
+  });
+
+  test("shuffle 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.shuffle).toBe("function");
+  });
+
+  test("pick 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.pick).toBe("function");
+  });
+
+  test("omit 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.omit).toBe("function");
+  });
+
+  test("memoize 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.memoize).toBe("function");
+  });
+
+  test("debounce 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.debounce).toBe("function");
+  });
+
+  test("throttle 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.throttle).toBe("function");
+  });
+
+  test("clickOutside 함수가 모듈에 포함되어 있는지 확인", () => {
+    expect(typeof _.clickOutside).toBe("function");
+  });
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.pick).toBe('function');
+test("fetch 함수가 정상적으로 동작하는지 확인(success 상황)", async () => {
+  _.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ test: 100 }),
+    })
+  ) as jest.Mock;
+
+  const data = await _.fetch("www.google.com").then((response) =>
+    response.json()
+  );
+
+  expect(data).toEqual({ test: 100 });
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.omit).toBe('function');
+test("fetch 함수가 정상적으로 동작하는지 확인(error 상황)", async () => {
+  _.fetch = jest.fn(() =>
+    Promise.reject(new Error("에러입니다."))
+  ) as jest.Mock;
+
+  const data = await _.fetch("www.google.com")
+    .then((response) => response.json())
+    .catch((e) => e);
+
+  expect(data).toEqual(new Error("에러입니다."));
 });
 
-test('Selector 동작 확인', () => {
-	const divElement = document.createElement('div');
-	divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
-	document.body.appendChild(divElement);
+describe("isNull 함수가 정상적으로 동작하는지 확인", () => {
+  test("isNull 함수 인자에 null값을 넣을 경우 true를 return 한다.", () => {
+    expect(_.isNull(null)).toBe(true);
+  });
 
-	const buttonElement = _('button.test-btn');
-	expect(buttonElement).toBeTruthy();
-
-	document.body.removeChild(buttonElement);
+  test("isNull 함수 인자에 null이 아닌 값을 넣을 경우 false를 return 한다.", () => {
+    expect(_.isNull(0)).toBe(false);
+    expect(_.isNull("")).toBe(false);
+  });
 });
 
-test('`_("").innerHTML()`~~~~', () => {});
+describe("isNil 함수가 정상적으로 동작하는지 확인", () => {
+  test("isNil 함수 인자에 null 또는 undefined 값을 넣을 경우 true를 return 한다.", () => {
+    expect(_.isNil(null)).toBe(true);
+    expect(_.isNil(undefined)).toBe(true);
+  });
 
-test('`_("").show()`~~~~', () => {});
+  test("isNull 함수 인자에 null 또는 undefined 가 아닌 값을 넣을 경우 false를 return 한다.", () => {
+    expect(_.isNil(2)).toBe(false);
+  });
+});
 
-test('`_("").hidden()`~~~~', () => {});
+describe("isNumber 함수가 정상적으로 동작하는지 확인", () => {
+  test("isNumber 함수 인자에 type이 number인 값이 들어갈 경우 true를 return 한다.", () => {
+    expect(_.isNumber(2)).toBe(true);
+  });
 
-test('`_("").addEvent()`~~~~', () => {});
+  test("isNumber 함수 인자에 type이 number가 아닌 값이 들어갈 경우 false를 return 한다.", () => {
+    expect(_.isNumber("2")).toBe(false);
+  });
+});
+
+describe("isFunction 함수가 정상적으로 동작하는지 확인", () => {
+  test("isFunction 함수 인자에 type이 function인 값이 들어갈 경우 true를 return 한다.", () => {
+    const testFunc = () => {};
+
+    expect(_.isFunction(testFunc)).toBe(true);
+  });
+
+  test("isFunction 함수 인자에 type이 function인 값이 들어갈 경우 true를 return 한다.", () => {
+    expect(_.isFunction(2)).toBe(false);
+  });
+});
+
+test("shuffle 함수가 정상적으로 동작하는지 확인", () => {
+  const testArray = [1, 2, 3, 4];
+  _.shuffle = jest.fn().mockReturnValue([3, 2, 1, 4]);
+
+  expect(_.shuffle(testArray)).toEqual([3, 2, 1, 4]);
+});
+
+test("pick 함수가 정상적으로 동작하는지 확인", () => {
+  const testArray = [1, 2, 3, 4];
+  const testObject = { a: 1, b: 2, c: 3, d: 4 };
+
+  expect(_.pick(testArray, [0, 1])).toEqual([1, 2]);
+  expect(_.pick(testObject, ["a", "b"])).toEqual({ a: 1, b: 2 });
+});
+
+test("omit 함수가 정상적으로 동작하는지 확인", () => {
+  const testArray = [1, 2, 3, 4];
+  const testObject = { a: 1, b: 2, c: 3, d: 4 };
+
+  expect(_.omit(testArray, [0, 1])).toEqual([3, 4]);
+  expect(_.omit(testObject, ["a", "b"])).toEqual({ c: 3, d: 4 });
+});
+
+test("memoize 함수가 정상적으로 동작하는지 확인", () => {
+  const testObject = { a: 1, b: 2 };
+  const memoized = _.memoize(() => testObject);
+
+  expect(memoized()).toEqual({ a: 1, b: 2 });
+});
+
+test("debounce 함수가 정상적으로 동작하는지 확인", () => {
+  const func = jest.fn();
+  const debouncedFunc = _.debounce(func, 100);
+
+  for (let i = 0; i < 1000; i++) {
+    debouncedFunc();
+  }
+
+  jest.runAllTimers();
+  expect(func).toHaveBeenCalledTimes(1);
+});
+
+test("throttle 함수가 정상적으로 동작하는지 확인", () => {
+  const func = jest.fn();
+  const throttledFunc = _.throttle(func, 1000);
+
+  for (let i = 0; i < 100; i++) {
+    throttledFunc();
+    jest.advanceTimersByTime(10);
+  }
+
+  jest.runAllTimers();
+  expect(func).toHaveBeenCalledTimes(1);
+});
+
+test("clickOutside 함수가 정상적으로 동작하는지 확인", () => {
+  const parentElement = document.createElement("div");
+  parentElement.innerHTML = `<button class='test-button'>Continue</button>`;
+  document.body.appendChild(parentElement);
+  const _instance = new _(".test-button");
+  const targetElement = _instance.get();
+
+  _.clickOutside(
+    parentElement,
+    targetElement,
+    (targetElement) => (targetElement.textContent = "clicked outer")
+  );
+
+  parentElement.click();
+  expect(targetElement.textContent).toBe("clicked outer");
+});
+
+test("Selector 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
+
+  const _instance = new _(".test-btn");
+  const buttonElement = _instance.get();
+
+  divElement.removeChild(buttonElement);
+
+  expect(_instance.get()).toBe(null);
+});
+
+test("innerHTML 메서드 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
+
+  const _instance = new _(".test-btn");
+  _instance.innerHTML(_instance.get(), `<div>동작확인</div>`);
+
+  expect(_instance.get().textContent).toBe("동작확인");
+});
+
+test("hidden 메서드 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
+
+  const _instance = new _(".test-btn");
+  const targetElement = _instance.get();
+  _instance.hidden(targetElement);
+
+  expect(targetElement.style.display).toBe("none");
+});
+
+test("show 메서드 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
+
+  const _instance = new _(".test-btn");
+  const targetElement = _instance.get();
+  _instance.show(targetElement);
+
+  expect(targetElement.style.display).toBe("");
+});
+
+test("addEvent 메서드 동작 확인", () => {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+  document.body.appendChild(divElement);
+
+  const _instance = new _(".test-btn");
+  const targetElement = _instance.get();
+  _instance.addEvent(targetElement, "click", () =>
+    _instance.innerHTML(targetElement, "<div>clickEvent</div>")
+  );
+
+  targetElement.click();
+  expect(targetElement.textContent).toBe("clickEvent");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,204 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+class _ {
+  selector: string;
+
+  constructor(selector: string) {
+    this.selector = selector;
+  }
+
+  get() {
+    return document.querySelector(this.selector) as HTMLElement;
+  }
+
+  innerHTML(element: HTMLElement, text: string) {
+    element.innerHTML = text;
+  }
+
+  show(element: HTMLElement) {
+    element.style.display = "";
+  }
+
+  hidden(element: HTMLElement) {
+    element.style.display = "none";
+  }
+
+  addEvent(
+    element: HTMLElement,
+    eventType: keyof HTMLElementEventMap,
+    callback: () => void
+  ) {
+    element.addEventListener(eventType, callback);
+  }
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch(
+    path: RequestInfo | URL,
+    config?: RequestInit
+  ): Promise<Response> {
+    return window
+      .fetch(path, config)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
 
-	export function isNull() {}
+        return response.json();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
-	export function isNil() {}
+  export function isNull(value: unknown): value is null {
+    return value === null;
+  }
 
-	export function isNumber() {}
+  export function isNil(value: unknown): value is undefined | null {
+    return typeof value === "undefined" || value === null;
+  }
 
-	export function isFunction() {}
+  export function isNumber(value: unknown): value is number {
+    return typeof value === "number";
+  }
 
-	export function shuffle() {}
+  export function isFunction(value: unknown): value is Function {
+    return typeof value === "function";
+  }
 
-	export function pick() {}
+  export function shuffle<T>(array: T[]): T[] {
+    return array.sort(() => Math.random() - 0.5);
+  }
 
-	export function omit() {}
+  export function pick<T, P>(
+    obj: Record<string | symbol | number, T> | T[],
+    rest: P[]
+  ): Record<string | symbol | number, T> | T[] {
+    if (Array.isArray(obj)) {
+      const pickArray: T[] = [];
 
-	export function memoize() {}
+      rest.forEach((restItem) => {
+        if (typeof restItem !== "number") return [];
 
-	export function debounce() {}
+        pickArray.push(obj.filter((objItem) => objItem === obj[restItem])[0]);
+      });
 
-	export function throttle() {}
+      return pickArray;
+    }
 
-	export function clickOutside() {}
+    const pickObject: Record<string | symbol | number, T> = {};
+    const objEntires = Object.entries(obj);
+
+    rest.forEach((restItem) => {
+      const [key, value] = objEntires.filter(
+        (objEntry) => objEntry[0] === restItem
+      )[0];
+
+      pickObject[key] = value;
+    });
+
+    return pickObject;
+  }
+
+  export function omit<T, P>(
+    obj: Record<string | symbol | number, T> | T[],
+    rest: P[]
+  ): Record<string | symbol | number, T> | T[] {
+    if (Array.isArray(obj)) {
+      const omitArray: T[] = [...obj];
+      let idx = 0;
+
+      while (rest.length > idx) {
+        const targetItem = rest[idx];
+        if (typeof targetItem !== "number") return [];
+
+        omitArray.splice(omitArray.indexOf(obj[targetItem]), 1);
+
+        idx++;
+      }
+
+      return omitArray;
+    }
+
+    const omitObject: Record<string | symbol | number, T> = {};
+    const objEntires = Object.entries(obj);
+    let idx = 0;
+
+    while (rest.length > idx) {
+      const targetItem = rest[idx];
+      objEntires.splice(
+        objEntires.findIndex((objEntry) => objEntry[0] === targetItem),
+        1
+      );
+
+      idx++;
+    }
+
+    objEntires.forEach((objEntry) => {
+      const [key, value] = objEntry;
+      omitObject[key] = value;
+    });
+
+    return omitObject;
+  }
+
+  export function memoize<T>(func: () => T): () => T {
+    const cache: { memoizedData: T | null } = {
+      memoizedData: null,
+    };
+
+    return function () {
+      if (cache.memoizedData) {
+        return cache.memoizedData;
+      }
+
+      cache.memoizedData = func();
+
+      return cache.memoizedData;
+    };
+  }
+
+  export function debounce<T, P extends any[]>(
+    callback: (...args: P) => T,
+    delay: number
+  ) {
+    let timerId: ReturnType<typeof setTimeout>;
+
+    return function (...args: P) {
+      if (timerId) clearTimeout(timerId);
+
+      timerId = setTimeout(() => callback(...args), delay);
+    };
+  }
+
+  export function throttle<T, P extends any[]>(
+    callback: (...args: P) => T,
+    delay: number
+  ) {
+    let timerId: ReturnType<typeof setTimeout> | null;
+
+    return function (...args: P) {
+      if (timerId) return;
+
+      timerId = setTimeout(() => {
+        callback(...args);
+        timerId = null;
+      }, delay);
+    };
+  }
+
+  export function clickOutside(
+    parentElement: HTMLElement,
+    targetElement: HTMLElement,
+    callback: (targetElement: HTMLElement) => void
+  ): void {
+    parentElement.addEventListener("click", (event: MouseEvent) => {
+      if (!targetElement.contains(event.target as HTMLElement)) {
+        callback(targetElement);
+
+        return;
+      }
+    });
+  }
 }
 
 export default _;
