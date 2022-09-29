c1ec9870abd278fc317368f9d445bff2cb3b0f48
Soyi Jeon <soyi.dev@gmail.com>
2022-09-29 17:40:33 +0900

    [TypeScript & Module Typing] 소피아(전소이) 미션 제출합니다. (#4)
    * chore: 설정 파일 추가 및 수정
    
    * test: 테스트 케이스 작성
    
    * feat: 타입 선언
    
    * chore: jest, tsd, tsconfig 설정 추가
    
    * feat: 로직 구현
    
    * chore: package.json script 수정
    
    * feat: isNumber 함수 업데이트
    
    * feat: .gitignore 업데이트
    
    * refactor: pick, omit 반환 타입 변경
    
    * chore: tsconfig.json 변경
    
    * fix: memoize 함수 인자 타입 변경
    
    * test: shuffle 테스트 코드 변경

--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -1,10 +1,73 @@
 /**
  * @jest-environment jsdom
  */
-import { expectType } from 'tsd';
+import { expectType } from "tsd";
 
-import _ from '../src';
+import _ from "../src";
 
-_('.button').addEvent('click', function (event) {
-	expectType<MouseEvent>(event);
+_(".button").addEvent("click", function (event) {
+  expectType<MouseEvent>(event);
 });
+
+expectType<Promise<Response>>(_.fetch("https://test.com"));
+expectType<Promise<Response>>(
+  _.fetch("https://test.com", {
+    method: "GET",
+  })
+);
+
+expectType<boolean>(_.isNull(null));
+expectType<boolean>(_.isNull(0));
+
+expectType<boolean>(_.isNil(undefined));
+expectType<boolean>(_.isNil(NaN));
+
+const numberArray = [1, 2, 3, 4];
+const stringArray = ["a", "b", "c"];
+const complexArray = ["a", 1, "b", 2, "c", 3];
+
+expectType<typeof numberArray>(_.shuffle<number>(numberArray));
+expectType<typeof stringArray>(_.shuffle<string>(stringArray));
+expectType<typeof complexArray>(_.shuffle(complexArray));
+
+expectType<{ a: number }>(_.pick({ a: 1, b: "c" }, ["a"]));
+
+expectType<{ b: string }>(_.omit({ a: 1, b: "c" }, ["a"]));
+
+expectType<(a: number, b: number) => number>(
+  _.memoize((a: number, b: number) => {
+    return a + b;
+  })
+);
+
+expectType<(a: number, b: number) => number>(
+  _.memoize(
+    (a: number, b: number) => {
+      return a + b;
+    },
+    (a: number) => a.toString()
+  )
+);
+
+expectType<(str1: string, str2: string) => number>(
+  _.memoize((str1: string, str2: string) => str1.length + str2.length)
+);
+
+expectType<(a: number, b: number) => void>(
+  _.debounce((a: number, b: number) => {
+    console.log(a, b);
+  }, 500)
+);
+
+expectType<(a: number, b: number) => void>(
+  _.throttle((a: number, b: number) => {
+    console.log(a, b);
+  }, 500)
+);
+
+const div = document.createElement("div");
+expectType<void>(
+  _.clickOutside(div, () => {
+    console.log("click outside");
+  })
+);
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,181 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+declare global {
+  interface HTMLElement extends CustomElementProperty {}
 }
 
+interface CustomElementProperty {
+  innerHTML: string;
+  hide: () => void;
+  show: () => void;
+  addEvent: <K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, event: HTMLElementEventMap[K]) => any
+  ) => void;
+}
+
+function _(selector: string) {
+  const element = document.querySelector<HTMLElement>(selector);
+  if (element === null) throw new Error("No Element");
+
+  const show = () => {
+    element.style.visibility = "visible";
+  };
+
+  const hide = () => {
+    element.style.visibility = "hidden";
+  };
+
+  const addEvent = <K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, event: HTMLElementEventMap[K]) => any
+  ) => {
+    element.addEventListener(type, listener);
+  };
+
+  element.show = show;
+  element.hide = hide;
+  element.addEvent = addEvent;
+
+  return element;
+}
+
+type Function<T = unknown, R = unknown> = (...args: T[]) => R;
+
+type FetchResource = string | URL | Request;
+
+type FetchOptions = {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  headers?: Headers;
+  body?:
+    | string
+    | Blob
+    | ArrayBuffer
+    | DataView
+    | FormData
+    | URLSearchParams
+    | ReadableStream;
+};
+
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch(
+    resource: FetchResource,
+    options?: FetchOptions
+  ): Promise<Response> {
+    return fetch(resource, options);
+  }
 
-	export function isNull() {}
+  export function isNull(value: unknown): boolean {
+    return value === null;
+  }
 
-	export function isNil() {}
+  export function isNil(value: unknown): boolean {
+    return value === null || value === undefined;
+  }
 
-	export function isNumber() {}
+  export function isNumber(value: unknown): boolean {
+    return typeof value === "number" || value instanceof Number;
+  }
 
-	export function isFunction() {}
+  export function isFunction(value: unknown): boolean {
+    return typeof value === "function";
+  }
 
-	export function shuffle() {}
+  export function shuffle<T>(array: T[]): T[] {
+    const length = array === null ? 0 : array.length;
+    if (!length) return [];
+    let index = -1;
+    const lastIndex = length - 1;
+    const result = [...array];
+    while (++index < length) {
+      const rand = index + Math.floor(Math.random() * (lastIndex - index + 1));
+      const value = result[rand];
+      result[rand] = result[index];
+      result[index] = value;
+    }
+    return result;
+  }
 
-	export function pick() {}
+  export function pick<T extends Record<string, unknown>, K extends keyof T>(
+    object: T,
+    paths: K[]
+  ): Pick<T, K> {
+    const result = paths.reduce(
+      (prev, cur) => ({ ...prev, [cur]: object[cur] }),
+      {} as T
+    );
+    return result;
+  }
 
-	export function omit() {}
+  export function omit<T extends Record<string, unknown>, K extends keyof T>(
+    object: T,
+    paths: K[]
+  ): Omit<T, K> {
+    const result = { ...object };
+    paths.forEach((key) => {
+      delete result[key];
+    });
+    return result;
+  }
 
-	export function memoize() {}
+  export function memoize<T, R>(
+    func: Function<T, R>,
+    resolver?: Function<
+      T,
+      string | number | boolean | null | undefined | symbol | bigint
+    >
+  ): Function<T, R> {
+    const memoized = function (args: any) {
+      const key = resolver ? resolver(args) : args[0];
+      const cache = new Map();
 
-	export function debounce() {}
+      if (cache.has(key)) {
+        return cache.get(key);
+      }
 
-	export function throttle() {}
+      const result = func(args);
+      cache.set(key, result);
+      return result;
+    };
+    return memoized;
+  }
 
-	export function clickOutside() {}
+  export function debounce<T, R>(
+    func: Function<T, R>,
+    wait: number
+  ): Function<T, void> {
+    let timerId: NodeJS.Timeout | null;
+
+    return (...args) => {
+      if (timerId) clearTimeout(timerId);
+      timerId = setTimeout(func, wait, ...args);
+    };
+  }
+
+  export function throttle<T, R>(
+    func: Function<T, R>,
+    wait: number
+  ): Function<T, void> {
+    let timerId: NodeJS.Timeout | null;
+
+    return (...args) => {
+      if (timerId) return;
+      timerId = setTimeout(() => {
+        func(...args);
+        timerId = null;
+      }, wait);
+    };
+  }
+
+  export function clickOutside(
+    target: Element,
+    handler: (event: MouseEvent) => any
+  ): void {
+    window.addEventListener("click", (event) => {
+      const isClickInside = target.contains(event.target as HTMLElement);
+      if (!isClickInside) {
+        handler(event);
+      }
+    });
+  }
 }
 
 export default _;
