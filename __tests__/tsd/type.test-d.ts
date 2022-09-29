6048816fbe0961ab01a1b47f2b0106ac9d29f002
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-09-29 13:23:46 +0900

    [TypeScript & Module Typing] 결(김윤진) 미션 제출합니다. (#14)
    * chore: 환경 설정
    
    * chore: jest 환경설정
    
    * chore: npm package 추가
    
    * chore: tsconfig 설정 추가
    
    * feat: 유틸 함수 추가
    
    * feat: api, type 테스트 추가
    
    * chore: 불필요 모듈 제거
    
    * feat: 유틸함수 수정
    
    * feat: 타입 테스트 수정

---
+++ __tests__/tsd/type.test-d.ts
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+import { expectType } from 'tsd';
+import _ from '../../src/index';
+// import HolaType from 'levellog-util-types';
+
+test('isNull 타입 테스트', () => {
+  expectType<boolean>(_.isNull('undefined'));
+});
+
+test('isNil 타입 테스트', () => {
+  expectType<boolean>(_.isNil('null'));
+});
+
+test('isNumber 타입 테스트', () => {
+  expectType<boolean>(_.isNumber(123));
+});
+
+test('isFunction 타입 테스트', () => {
+  expectType<boolean>(_.isFunction(() => {}));
+});
+
+test('shuffle 타입 테스트', () => {
+  expectType<number[]>(_.shuffle<number>([1, 2, 3]));
+});
+
+test('pick 타입 테스트', () => {
+  expectType<Array<{ [props: string]: any }>>(_.pick(['a', 'b'], { a: 1, b: 2, c: 3, e: 4 }));
+});
+
+test('omit 타입 테스트', () => {
+  expectType<Array<{ [props: string]: any }>>(_.omit(['a', 'b'], { a: 1, b: 2, c: 3, e: 4 }));
+});
+
+test('memoize 타입 테스트', <T>() => {
+  expectType<T[]>(_.memoize<T>((args: any[]) => [])());
+});
+
+test('debounce 타입 테스트', () => {
+  expectType(_.debounce.action({ func: () => {} }));
+});
+
+test('throttle 타입 테스트', () => {
+  expectType(_.throttle(() => {}, 100));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,169 @@
 function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+  function getElement() {
+    const el = document.getElementById(selector) as HTMLElement;
+
+    return el;
+  }
+
+  function innerHTML(template?: string) {
+    const el = getElement();
+
+    if (template) {
+      el.innerHTML = template;
+      return;
+    }
+
+    return el.innerHTML;
+  }
+
+  function show() {
+    const el = getElement();
+
+    el.style.display = '';
+  }
+
+  function hidden() {
+    const el = getElement();
+
+    el.style.display = 'none';
+  }
+
+  function addEvent(type: keyof HTMLElementEventMap, listener: EventListenerOrEventListenerObject) {
+    const el = getElement();
+
+    el.addEventListener(type, listener);
+  }
+
+  return {
+    innerHTML,
+    show,
+    hidden,
+    addEvent,
+  };
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export function fetch(path: RequestInfo | URL, config?: RequestInit): Promise<Response> {
+    return window.fetch(path, config).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    });
+  }
 
-	export function isNull() {}
+  type Nullable<Type> = Type extends null ? Type : never;
+  export function isNull<Type>(args: Type): args is Nullable<Type> {
+    if (args === null) {
+      return true;
+    }
 
-	export function isNil() {}
+    return false;
+  }
 
-	export function isNumber() {}
+  type Nilable<Type> = Type extends null | undefined ? Type : never;
+  export function isNil<Type>(args: Type): args is Nilable<Type> {
+    if (args === null || args === undefined) {
+      return true;
+    }
 
-	export function isFunction() {}
+    return false;
+  }
 
-	export function shuffle() {}
+  export function isNumber(args: unknown): args is number {
+    return typeof args === 'number';
+  }
 
-	export function pick() {}
+  isNumber('zzzz');
 
-	export function omit() {}
+  export function isFunction(args: unknown): args is Function {
+    return typeof args === 'function';
+  }
 
-	export function memoize() {}
+  export function shuffle<T>(args: T[]): T[] {
+    let arr = [];
+    let n: number = args.length;
+    let i: number;
+    while (n) {
+      i = Math.floor(Math.random() * n--);
+      arr.push(args.splice(i, 1)[0]);
+    }
 
-	export function debounce() {}
+    return arr;
+  }
 
-	export function throttle() {}
+  export function pick(arr: Array<string>, obj: Record<string, any>): Array<Pick<typeof obj, typeof arr[number]>> {
+    return Object.entries(obj)
+      .filter(([k, _]) => arr.includes(k))
+      .map(([k, v]) => ({ [k]: v }));
+  }
 
-	export function clickOutside() {}
+  export function omit(arr: Array<string>, obj: Record<string, any>): Array<Omit<typeof obj, typeof arr[number]>> {
+    return Object.entries(obj)
+      .filter(([k, _]) => !arr.includes(k))
+      .map(([k, v]) => ({ [k]: v }));
+  }
+
+  const cache: CacheType = { time: 0, data: [] };
+  export function memoize<T>(func: (args: any[]) => T[], timer: number = 50000): Function {
+    if (!isFunction(func)) {
+      throw new TypeError('Expected a function');
+    }
+
+    const memo = function (...args: any[]): T[] {
+      const curTime = new Date().getTime();
+      if (cache.time && cache.time + timer > curTime) {
+        return cache.data;
+      }
+      const result: T[] = func(args);
+      cache.time = curTime;
+      cache.data.splice(0, 1, result);
+
+      return cache.data;
+    };
+
+    return memo;
+  }
+
+  interface CacheType {
+    time: number;
+    data: any[];
+  }
+
+  export const debounce: DebounceType = {
+    flag: 0,
+    action({ func, args }) {
+      if (this.flag) {
+        clearTimeout(this.flag);
+      }
+      this.flag = setTimeout(() => {
+        args ? func({ ...args }) : func();
+      }, 300);
+    },
+  };
+
+  interface DebounceActionArgsType<P, R> {
+    func: (args?: P) => R;
+    args?: P;
+  }
+
+  interface DebounceType {
+    flag: number | ReturnType<typeof setTimeout>;
+    action: <P, R>({ func, args }: DebounceActionArgsType<P, R>) => void;
+  }
+
+  let flag = true;
+  export function throttle(func: Function, time: number) {
+    if (flag) {
+      flag = false;
+      func();
+    }
+    setTimeout(() => {
+      flag = true;
+    }, time);
+  }
+
+  export function clickOutside() {}
 }
 
 export default _;
