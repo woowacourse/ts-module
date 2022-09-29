63630bd626dd2bcc67369c20ce4b8a9459b9ab8b
soobin <38878617+sanaandmomo@users.noreply.github.com>
2022-09-29 16:10:46 +0900

    [TypeScript & Module Typing] 아놀드(한수빈) 미션 제출합니다. (#10)
    * build: tsconfig 설정
    
    * chore: jest 환경 설정
    
    * chore: gitignore 설정
    
    * feat: numberRange 만드는 유틸 타입
    
    * feat: fetch 타입 선언
    
    * test: fetch 타입 테스트 작성
    
    * feat: isNull 타입 선언
    
    * test: isNull 타입 테스트
    
    * feat: isNil 타입 선언
    
    * test: isNil 타입 테스트 작성
    
    * feat: isNumber 타입 선언
    
    * test: isNumber 타입 테스트 작성
    
    * feat: shuffle 타입 선언
    
    * test: shuffle 타입 테스트
    
    * feat: pick 타입 선언
    
    * test: pick 타입 테스트
    
    * feat: omit 타입 선언
    
    * test: omit 타입 테스트
    
    * feat: memoize 타입 선언
    
    * test: memoize 타입 테스트
    
    * feat: debounce 타입 선언
    
    * test: debounce 타입 테스트
    
    * feat: throttle 타입 선언
    
    * test: throttle 타입 테스트
    
    * feat: clickOutside 타입 선언
    
    * test: clickOutside 타입 테스트
    
    * feat: Node 인터페이스 확장
    
    * test: Node addEvent 타입 테스트
    
    * test: Node show, hidden 타입 테스트
    
    * test: Node innerHTML 타입 테스트
    
    * feat: fetch 함수의 인자 url 타입 리터럴 타입으로 수정
    
    * feat: isNull 함수 구현
    
    * feat: isNil 함수 구현
    
    * feat: isNumber 함수 구현
    
    * feat: isFunction 함수 구현
    
    * feat: shuffle 함수 구현
    
    * feat: pick 함수 구현
    
    * feat: memoize 구현
    
    * feat: debounce 구현
    
    * feat: throttle 구현
    
    * clickOutside 구현
    
    * build: 출력물 경로 수정
    
    * build: d.ts 파일만 생성하도록 설정
    
    * feat: dom util 타입 리팩터링 및 테스트 코드 수정
    
    * feat: fetch 타입 선언
    
    * test: 테스트 코드 test 함수로 래핑
    
    * refactor: 중복되는 함수 타입 제거
    
    * refactor: thrrotle 함수의 arg 타입을 일치시키기
    
    * feat: memoize, debounce, throttle args 타입 동기화
    
    * test: api 테스트
    
    * refactor: memoize, debounce, throttle 함수 타입 Parameters 를 통해 일관성 맞추기

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,217 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+function _(selector: string) {
+  const element = document.createElement(selector);
+
+  if (element === undefined) {
+    throw Error('Element를 생성하는데 실패하였습니다.');
+  }
+
+  function getElement(): HTMLElement {
+    return element;
+  }
+
+  function innerHtml(content?: string): string {
+    if (content !== undefined) {
+      element.innerHTML = content;
+    }
+
+    return element.innerHTML;
+  }
+
+  function show() {
+    element.style.display = 'block';
+  }
+
+  function hide() {
+    element.style.display = 'none';
+  }
+
+  function addEvent<T extends keyof GlobalEventHandlersEventMap>(
+    type: T,
+    listener: (e: GlobalEventHandlersEventMap[T]) => void
+  ) {
+    element.addEventListener(type, listener);
+  }
+
+  return {
+    getElement,
+    innerHtml,
+    show,
+    hide,
+    addEvent,
+  };
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  type FetchBodyType =
+    | string
+    | URLSearchParams
+    | FormData
+    | Blob
+    | ArrayBuffer
+    | ArrayBufferView
+    | DataView;
 
-	export function isNull() {}
+  type Url = `http${'' | 's'}://${string}`;
 
-	export function isNil() {}
+  type FetchOptions = {
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    body?: FetchBodyType | null;
+    headers?: Record<string, string>;
+    credentials?: 'omit' | 'same-origin' | 'include';
+  };
 
-	export function isNumber() {}
+  type Response<Data> = {
+    status: number;
+    ok: boolean;
+    statusText: string;
+    url?: string;
+    headers?: Record<string, string>;
+    json: () => Promise<Data>;
+  };
 
-	export function isFunction() {}
+  export function fetch<Data>(
+    url: Url,
+    options?: FetchOptions
+  ): Promise<Response<Data>> {
+    return new Promise((resolve, reject) => {
+      const response: Response<Data> = {
+        status: 200,
+        ok: true,
+        statusText: 'example code',
+        json: () => {
+          return new Promise((resolve, reject) => {
+            resolve('아무튼 데이터임..!' as unknown as Data);
+          });
+        },
+      };
 
-	export function shuffle() {}
+      resolve(response);
+    });
+  }
 
-	export function pick() {}
+  type Nullable<T> = T extends null ? T : never;
+  export function isNull<T>(value: T): value is Nullable<T> {
+    return value === null;
+  }
 
-	export function omit() {}
+  type Nilable<T> = T extends null | undefined ? T : never;
+  export function isNil<T>(value: T): value is Nilable<T> {
+    return isNull(value) || value === undefined;
+  }
 
-	export function memoize() {}
+  type Numberable<T> = T extends number ? T : never;
+  export function isNumber<T>(value: T): value is Numberable<T> {
+    return typeof value === 'number';
+  }
 
-	export function debounce() {}
+  type Functionable<T> = T extends Function ? T : never;
+  export function isFunction<T>(value: T): value is Functionable<T> {
+    return typeof value === 'function';
+  }
 
-	export function throttle() {}
+  export function shuffle<T>(value: T[]): T[] {
+    const length = value == null ? 0 : value.length;
 
-	export function clickOutside() {}
+    if (!length) {
+      return [];
+    }
+
+    let index = -1;
+    const lastIndex = length - 1;
+    const result = [...value];
+
+    while (++index < length) {
+      const rand = index + Math.floor(Math.random() * (lastIndex - index + 1));
+      const value = result[rand];
+      result[rand] = result[index];
+      result[index] = value;
+    }
+
+    return result;
+  }
+
+  export function pick<T extends Record<string, unknown>, R extends keyof T>(
+    obj: T,
+    ...keys: R[]
+  ): Pick<T, R> {
+    return keys.reduce((acc, key) => {
+      acc[key] = obj[key];
+
+      return acc;
+    }, {} as Pick<T, R>);
+  }
+
+  export function omit<T extends Record<string, unknown>, R extends keyof T>(
+    obj: T,
+    ...keys: R[]
+  ): Omit<T, R> {
+    return keys.reduce(
+      (acc, key) => {
+        delete acc[key];
+
+        return acc;
+      },
+      { ...obj }
+    );
+  }
+
+  export function memoize<T extends (...args: any[]) => any>(
+    func: T,
+    makeKey: (...args: Parameters<T>) => string
+  ): (...args: Parameters<T>) => any {
+    const cache: Record<string, any> = {};
+
+    return (...args: Parameters<T>): any => {
+      const key = makeKey(...args);
+
+      return cache[key] ? cache[key] : (cache[key] = func(...args));
+    };
+  }
+
+  export function debounce<T extends (...args: unknown[]) => void>(
+    func: T,
+    wait: number
+  ): (...args: Parameters<T>) => void {
+    let timer: ReturnType<typeof setTimeout> | null = null;
+
+    return (...args: Parameters<T>): void => {
+      if (timer) clearTimeout(timer);
+
+      timer = setTimeout(() => func(...args), wait);
+    };
+  }
+
+  export function throttle<T extends (...args: unknown[]) => void>(
+    func: T,
+    wait: number
+  ): (...args: Parameters<T>) => void {
+    let timer: ReturnType<typeof setTimeout> | null = null;
+
+    return (...args: Parameters<T>) => {
+      if (!timer) {
+        timer = setTimeout(() => {
+          timer = null;
+          func(...args);
+        }, wait);
+      }
+    };
+  }
+
+  export function clickOutside(
+    dom: HTMLElement,
+    func: (...args: any[]) => void
+  ): void {
+    window.addEventListener('click', (e, ...args) => {
+      if (!(e.target instanceof HTMLElement)) {
+        return;
+      }
+      if (e.target === dom) {
+        return;
+      }
+
+      func(...args);
+    });
+  }
 }
 
 export default _;
