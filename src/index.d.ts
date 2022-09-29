4c34994f641cc146de3b06c31ac62ab6f9a92e5f
KIM YEJI <rladpwl0512@gmail.com>
2022-09-29 17:31:32 +0900

    feat: index.d.ts 빌드

--- src/index.d.ts
+++ src/index.d.ts
@@ -10,14 +10,15 @@ interface CustomElement {
 }
 declare function _(selector: string): HTMLElement | undefined;
 declare module _ {
-    function fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
+    function fetchData(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
     function isNull<T>(value: T): T extends null ? true : false;
     function isNil<T>(value: T): T extends null | undefined ? true : false;
     function isNumber<T>(value: T): T extends number ? true : false;
     function isFunction<T>(value: T): T extends Function ? true : false;
     function shuffle<T>(value: Array<T>): Array<T>;
-    function pick<T extends Record<string, unknown>, K extends keyof T>(obj: T, keys?: Array<K>): Pick<T, K> | {};
+    function pick<T extends Record<string, unknown>, K extends keyof T>(obj: T, keys: Array<K>): Pick<T, K>;
     function omit<T extends Record<string, unknown>, K extends keyof T>(obj: T, keys?: Array<K>): Omit<T, K>;
+    function memoize(func: Function): () => void;
     function debounce<Params extends unknown[]>(func: (...args: Params) => unknown, wait: number): (...args: Params) => unknown;
     function throttle<Params extends unknown[]>(func: (...args: Params) => unknown, wait: number): (...args: Params) => unknown;
     function clickOutside(elem: HTMLElement, func: Function): void;
