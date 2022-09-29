3ab60d571eddb558e8be1ea24b299714aff3a18a
juunzzi <78349600+juunzzi@users.noreply.github.com>
2022-09-29 17:11:55 +0900

    [TypeScript & Module Typing] 준찌(장준혁) 미션 제출합니다. (#26)
    * feat: 기능 구현 및 API 테스트 완료
    
    * test: 타입 테스트 추가
    
    * test: isFunction API 테스트 추가
    
    * feat: API 구현부 수정
    
    * feat: memoize 함수의 인자는 원본 함수의 인자와 동일한 타입을 갖는다.
    
    * test: mockedFetch 불필요한 Type Assertion 제거
    
    * refactor: fetch의 제너릭 타입을 Record 타입으로 한정
    
    * refactor: isFunction은 함수 타입인지를 검증하는 함수이다.
    
    * refactor: Array<U> -> U[]로 변경 하나로 통일한다

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,190 @@
-function _(selector: string): any {
-	/**
-	 * innerHTML() {
-	 * }
-	 *
-	 * show() {
-	 * }
-	 *
-	 * hidden() {
-	 * }
-	 *
-	 * addEvent() {
-	 * }
-	 */
+function keys<T extends Record<string, unknown>, U extends keyof T>(
+  object: T
+): U[] {
+  const keys: unknown = Object.keys(object);
+  return keys as U[];
+}
+
+function _(selector: string): {
+  get: () => HTMLElement;
+
+  innerHTML: (template: string) => void;
+
+  show: () => void;
+
+  hidden: () => void;
+
+  addEvent: <K extends keyof HTMLElementEventMap>(
+    type: K,
+    handler: (this: Element, ev: HTMLElementEventMap[K]) => void
+  ) => void;
+} {
+  const element = document.querySelector(selector) as HTMLElement;
+
+  if (element === null) {
+    throw new Error("엘리먼트를 찾지 못하였습니다");
+  }
+
+  return {
+    get() {
+      return element;
+    },
+    innerHTML(template) {
+      element.innerHTML = template;
+    },
+    show() {
+      element.style.display = "";
+    },
+    hidden() {
+      element.style.display = "none";
+    },
+    addEvent(type, handler) {
+      element.addEventListener(type, handler);
+    },
+  };
 }
 
 module _ {
-	export function fetch() {
-		return {};
-	}
+  export async function fetch<T extends Record<keyof T, unknown>>(
+    input: URL | RequestInfo,
+    init?: RequestInit | undefined
+  ): Promise<T> {
+    const response = await window.fetch(input, init);
 
-	export function isNull() {}
+    if (!response.ok) {
+      throw new Error("에러");
+    }
 
-	export function isNil() {}
+    const json: T = await response.json();
 
-	export function isNumber() {}
+    return json;
+  }
 
-	export function isFunction() {}
+  export function isNull(arg: unknown): arg is null {
+    return arg === null;
+  }
 
-	export function shuffle() {}
+  export function isNil(arg: unknown): arg is null | undefined {
+    return arg == null;
+  }
 
-	export function pick() {}
+  export function isNumber(arg: unknown): arg is number {
+    return typeof arg === "number";
+  }
 
-	export function omit() {}
+  export function isFunction(arg: unknown): arg is Function {
+    return typeof arg === "function";
+  }
 
-	export function memoize() {}
+  export function shuffle<T>(
+    list: T[] | null | undefined
+  ): T[] | null | undefined {
+    return list?.sort(() => Math.random() - 0.5);
+  }
 
-	export function debounce() {}
+  export function pick<T extends Record<string, unknown>, U extends keyof T>(
+    object: T,
+    ...props: U[]
+  ): Pick<T, U> {
+    return props.reduce<Pick<T, U>>(
+      (prev, currentKey) => ({
+        ...prev,
+        [currentKey]: object[currentKey],
+      }),
+      {} as Pick<T, U>
+    );
+  }
 
-	export function throttle() {}
+  export function omit<T extends Record<string, unknown>, U extends keyof T>(
+    object: T,
+    ...props: U[]
+  ): Omit<T, U> {
+    return keys<typeof object, U>(object).reduce<Omit<T, U>>(
+      (prev, currentKey) => {
+        if (props.includes(currentKey)) {
+          return prev;
+        }
+        return { ...prev, [currentKey]: object[currentKey] };
+      },
+      {} as Omit<T, U>
+    );
+  }
 
-	export function clickOutside() {}
+  export function memoize<T, R extends unknown[]>(
+    callbackFunction: (...args: R) => T
+  ): (...args: R) => T {
+    const memo: {
+      cache: { isMemoized: false; data: null } | { isMemoized: true; data: T };
+    } = {
+      cache: {
+        isMemoized: false,
+        data: null,
+      },
+    };
+
+    const memoizedFunction = (...args: R): T => {
+      if (!memo.cache.isMemoized) {
+        memo.cache = {
+          isMemoized: true,
+          data: callbackFunction(...args),
+        };
+      }
+
+      return memo.cache.data;
+    };
+
+    return memoizedFunction;
+  }
+
+  export function debounce(
+    callbackFunction: (...args: unknown[]) => void,
+    time: number
+  ): (...args: unknown[]) => void {
+    const debounce: {
+      timeout: NodeJS.Timeout | null;
+    } = {
+      timeout: null,
+    };
+
+    const debouncedFunction = (...args: unknown[]) => {
+      if (debounce.timeout) {
+        clearTimeout(debounce.timeout);
+      }
+
+      debounce.timeout = setTimeout(() => {
+        callbackFunction(...args);
+        debounce.timeout = null;
+      }, time);
+    };
+
+    return debouncedFunction;
+  }
+
+  export function throttle(
+    callbackFunction: (...args: unknown[]) => void,
+    time: number
+  ): (...args: unknown[]) => void {
+    const throttle: {
+      timeout: NodeJS.Timeout | null;
+    } = {
+      timeout: null,
+    };
+
+    const throttledFunction = (...args: unknown[]) => {
+      if (throttle.timeout) {
+        return;
+      }
+
+      throttle.timeout = setTimeout(() => {
+        callbackFunction(...args);
+        throttle.timeout = null;
+      }, time);
+    };
+
+    return throttledFunction;
+  }
+
+  export function clickOutside() {}
 }
 
 export default _;
