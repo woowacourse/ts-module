212425816f09c9914f1b72070184fdbe38a94abd
DomHardy <zuzudnf@gmail.com>
2022-09-29 16:12:40 +0900

    [TypeScript & Module Typing] 돔하디(이동현) 미션 제출합니다. (#21)
    * config : Jest, typescript, package 설정
    
    * chore : .gitignore 작성
    
    * test : api 테스트 초안 작성
    
    * test : lib 함수 타입 테스트 작성
    
    * feat : lib 함수 구현
    
    * feat : DOM util 함수 구현
    
    * test : innerHTML 동작 확인 테스트 작성
    
    * test : show 테스트 코드 작성
    
    * test : hidden 동작 테스트 작성
    
    * feat : addEvent 구현
    
    * test : addEvent 타입 테스트 작성
    
    * test : addEvent 동작확인 테스트 작성 및 리팩토링
    
    * refactor : test afterEach 로 반복작업 개선
    
    * refactor : show test 개선
    
    * refactor : pick, omit test 타입 개선
    
    * config : 불필요한 declaration 제거
    
    - module 확장성을 위해 commonjs 로 수정

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,39 +1,107 @@
 /**
  * @jest-environment jsdom
  */
-import _ from '../src';
+import _ from "../src";
 
-test('모듈은 기본 내보내기', () => {
-	expect(_).toBeTruthy();
+describe("모듈 내보내기 및 함수 테스트", () => {
+  test("모듈은 기본 내보내기", () => {
+    expect(_).toBeTruthy();
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.fetch).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.isNull).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.isNil).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.isNumber).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.isFunction).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.shuffle).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.pick).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.omit).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.memoize).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.debounce).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.throttle).toBe("function");
+  });
+
+  test("모듈에 포함된 함수 확인", () => {
+    expect(typeof _.clickOutside).toBe("function");
+  });
 });
 
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.fetch).toBe('function');
+describe("function _ 동작 확인 테스트", () => {
+  let selectedElement: HTMLElement;
+
+  beforeEach(() => {
+    const divElement = document.createElement("div");
+    divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
+    document.body.appendChild(divElement);
+
+    selectedElement = _("button.test-btn").element;
+  });
+
+  afterEach(() => {
+    selectedElement.remove();
+  });
+
+  test("Selector 동작 확인", () => {
+    expect(selectedElement).toBeTruthy();
+  });
+
+  test('_("").innerHTML() 동작 확인', () => {
+    _("button.test-btn").innerHTML(`<div id='domMorello'>TEST</div>`);
+
+    expect(_("div#domMorello").element).toBeTruthy();
+  });
+
+  test('_("").show() 동작 확인', () => {
+    _("button.test-btn").element.style.display = "none";
+    _("button.test-btn").show();
+
+    expect(selectedElement.style.display).toBe("block");
+  });
+
+  test('_("").hidden() 동작 확인', () => {
+    _("button.test-btn").hidden();
+
+    expect(selectedElement.style.display).toBe("none");
+  });
+
+  test('("").addEvent() 동작 확인', () => {
+    _("button.test-btn").addEvent("click", () => {
+      _("button.test-btn").hidden();
+    });
+
+    selectedElement.click();
+
+    expect(selectedElement.style.display).toBe("none");
+  });
 });
-
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.pick).toBe('function');
-});
-
-test('모듈에 포함된 함수 확인', () => {
-	expect(typeof _.omit).toBe('function');
-});
-
-test('Selector 동작 확인', () => {
-	const divElement = document.createElement('div');
-	divElement.innerHTML = `<button class='test-btn'>Continue</button>`;
-	document.body.appendChild(divElement);
-
-	const buttonElement = _('button.test-btn');
-	expect(buttonElement).toBeTruthy();
-
-	document.body.removeChild(buttonElement);
-});
-
-test('`_("").innerHTML()`~~~~', () => {});
-
-test('`_("").show()`~~~~', () => {});
-
-test('`_("").hidden()`~~~~', () => {});
-
-test('`_("").addEvent()`~~~~', () => {});
--- __tests__/type.test.ts
+++ __tests__/type.test.ts
@@ -1,10 +1,130 @@
 /**
  * @jest-environment jsdom
  */
-import { expectType } from 'tsd';
+import { expectType } from "tsd";
+import { FetchOptions, FetchResponse } from "types";
 
-import _ from '../src';
+import _ from "../src";
 
-_('.button').addEvent('click', function (event) {
-	expectType<MouseEvent>(event);
+describe("function _ type test", () => {
+  test('_("").addEvent type test', () => {
+    const button = document.createElement("button");
+    button.setAttribute("class", "button");
+    document.body.appendChild(button);
+
+    _(".button").addEvent("click", function (this, event) {
+      expectType<MouseEvent>(event);
+    });
+
+    button.remove();
+  });
+});
+
+describe("module _ type test", () => {
+  test("fetch test", () => {
+    type TestData = {
+      name: string;
+      age: number;
+    };
+
+    expectType<
+      (
+        entry: string,
+        options?: FetchOptions
+      ) => Promise<FetchResponse<TestData[]>>
+    >(_.fetch<TestData[]>);
+  });
+
+  test("isNull test", () => {
+    expectType<boolean>(_.isNull(null));
+    expectType<boolean>(_.isNull("hello"));
+  });
+
+  test("isNil test", () => {
+    expectType<boolean>(_.isNil(null));
+    expectType<boolean>(_.isNil(undefined));
+    expectType<boolean>(_.isNil("hello"));
+  });
+
+  test("isNumber test", () => {
+    expectType<boolean>(_.isNumber(2));
+    expectType<boolean>(_.isNumber("hello"));
+  });
+
+  test("isFunction test", () => {
+    expectType<boolean>(_.isFunction(() => console.log("hi")));
+    expectType<boolean>(_.isFunction("hello"));
+  });
+
+  test("shuffle test", () => {
+    const a = [1, "string", false];
+
+    type AType = typeof a;
+
+    expectType<AType>(_.shuffle(a));
+  });
+
+  test("pick test", () => {
+    const a = {
+      name: "dom",
+      age: 30,
+      married: false,
+    };
+
+    expectType<Pick<typeof a, "name">>(_.pick(a, "name"));
+  });
+
+  test("omit test", () => {
+    const a = {
+      name: "dom",
+      age: 30,
+      married: false,
+    };
+
+    expectType<Omit<typeof a, "name">>(_.omit(a, "name"));
+  });
+
+  test("memoize test", () => {
+    const func = (a: number) => console.log("hi");
+    const resolver = (a: number) => false;
+
+    type FuncType = typeof func;
+
+    expectType<FuncType>(_.memoize(func));
+    expectType<FuncType>(_.memoize(func, resolver));
+  });
+
+  test("debounce test", () => {
+    const func = (a: number) => 500;
+    const wait = 2000;
+    const options = {
+      leading: true,
+      maxWait: 1000,
+      trailing: false,
+    };
+
+    expectType<_.DebouncedFunctionType<[a: number]>>(
+      _.debounce(func, wait, options)
+    );
+  });
+
+  test("throttle test", () => {
+    const func = (a: number) => 500;
+    const wait = 2000;
+    const options = {
+      leading: true,
+      maxWait: 1000,
+      trailing: false,
+    };
+
+    expectType<_.DebouncedFunctionType<[a: number]>>(
+      _.throttle(func, wait, options)
+    );
+  });
+
+  test("clickOutside test", () => {
+    expectType<
+      (eventTarget: HTMLElement, innerElement: HTMLElement) => boolean
+    >(_.clickOutside);
+  });
 });
